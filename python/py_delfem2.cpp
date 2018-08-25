074f6fee1baa2f7f2aaa25c02b52b19538bf8064
Nobuyuki Umetani <n.umetani@gmail.com>
2018-08-25 12:07:51 +0900

    small neatify

--- python/py_delfem2.cpp
+++ python/py_delfem2.cpp
@@ -83,24 +83,8 @@ CMeshElem MeshQuad3D_VoxelGrid(const CVoxelGrid& vg){
 namespace py = pybind11;
 
 PYBIND11_MODULE(dfm2, m) {
-  m.doc() = "pybind11 example plugin";
+  m.doc() = "pybind11 delfem2 binding";
   //////
-  py::class_<CMeshElem>(m, "MeshElem")
-  .def(py::init<>())
-  .def(py::init<const std::string&>())
-  .def("read", &CMeshElem::Read)
-  .def("drawFace_elemWiseNorm", &CMeshElem::DrawFace_ElemWiseNorm)
-  .def("drawEdge", &CMeshElem::DrawEdge)
-  .def("scaleXYZ",&CMeshElem::ScaleXYZ)
-  .def("subdiv",  &CMeshElem::Subdiv)
-  .def_readonly("listElem", &CMeshElem::aElem)
-  .def_readonly("listPos",  &CMeshElem::aPos)
-  .def_readonly("elemType", &CMeshElem::elem_type);
-  
-  py::class_<CVoxelGrid>(m, "VoxelGrid")
-  .def(py::init<>())
-  .def("add",&CVoxelGrid::Add);
-  
   py::enum_<MESHELEM_TYPE>(m, "FemElemType")
   .value("Tri",     MESHELEM_TYPE::MESHELEM_TRI)
   .value("Quad",    MESHELEM_TYPE::MESHELEM_QUAD)
@@ -110,7 +94,23 @@ PYBIND11_MODULE(dfm2, m) {
   .value("Hex",     MESHELEM_TYPE::MESHELEM_HEX)
   .export_values();
   
-  m.def("setSomeLighting", &setSomeLighting);
+  py::class_<CMeshElem>(m, "MeshElem")
+  .def(py::init<>())
+  .def(py::init<const std::string&>())
+  .def("read", &CMeshElem::Read)
+  .def("drawFace_elemWiseNorm", &CMeshElem::DrawFace_ElemWiseNorm)
+  .def("drawEdge", &CMeshElem::DrawEdge)
+  .def("scaleXYZ", &CMeshElem::ScaleXYZ)
+  .def("subdiv",   &CMeshElem::Subdiv)
+  .def_readonly("listElem", &CMeshElem::aElem)
+  .def_readonly("listPos",  &CMeshElem::aPos)
+  .def_readonly("elemType", &CMeshElem::elem_type);
+  
+  py::class_<CVoxelGrid>(m, "VoxelGrid")
+  .def(py::init<>())
+  .def("add",&CVoxelGrid::Add);
   m.def("meshQuad3d_voxelGrid", &MeshQuad3D_VoxelGrid);
+  
+  m.def("setSomeLighting", &setSomeLighting);
 }
 
