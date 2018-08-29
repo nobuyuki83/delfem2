b74e317a468aedff8723ceaf41bbebc169f2e5bd
Nobuyuki Umetani <n.umetani@gmail.com>
2018-08-29 16:06:20 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='delfem2',
+      version='0.0.0',
+      description='a codebase for geometry processing and finite element method simulation',
+      author='Nobuyuki Umetani',
+      author_email='n.umetani@gmail.com',
+      url='http://nobuyuki.umetani.com')
