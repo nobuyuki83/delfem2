8454dc32a6bc9887d0764ead98a6635eaf1ec5b6
Nobuyuki Umetani <n.umetani@gmail.com>
2018-08-29 13:49:26 +0900

    python binding winDraw3d function and first Jupyter notebook article

--- python/py_delfem2.cpp
+++ python/py_delfem2.cpp
@@ -8,6 +8,7 @@
 //#include "delfem2/funcs_glut.h"
 
 #include "delfem2/mshtopoio_gl.h"
+#include "delfem2/bv.h"
 
 // TODO:Make a wrapper class of the VoxelGrid?
 CMeshElem MeshQuad3D_VoxelGrid(const CVoxelGrid& vg){
@@ -22,41 +23,14 @@ CMeshElem MeshQuad3D_VoxelGrid(const CVoxelGrid& vg){
 
 namespace py = pybind11;
 
-py::array_t<double> add_arrays
-(py::array_t<double> input1,
- py::array_t<double> input2)
-{
-  auto buf1 = input1.request(), buf2 = input2.request();
-  
-  if (buf1.size != buf2.size)
-    throw std::runtime_error("Input shapes must match");
-  
-  /*  allocate the buffer */
-  py::array_t<double> result = py::array_t<double>(buf1.size);
-  
-  auto buf3 = result.request();
-  
-  double *ptr1 = (double *) buf1.ptr,
-  *ptr2 = (double *) buf2.ptr,
-  *ptr3 = (double *) buf3.ptr;
-  int X = buf1.shape[0];
-  int Y = buf1.shape[1];
-  
-  for (size_t idx = 0; idx < X; idx++)
-    for (size_t idy = 0; idy < Y; idy++)
-      ptr3[idx*Y + idy] = ptr1[idx*Y+ idy] + ptr2[idx*Y+ idy];
-  
-  // reshape array to match input shape
-  result.resize({X,Y});
-  
-  return result;
-}
-
-
 void init_mshtopoio_gl(py::module &m);
 
 PYBIND11_MODULE(_dfm2, m) {
   m.doc() = "pybind11 delfem2 binding";
+  // aabb
+  py::class_<CBV3D_AABB>(m,"AABB")
+  .def(py::init<>());
+  
   // mesh
   init_mshtopoio_gl(m);
   
