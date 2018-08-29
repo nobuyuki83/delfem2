cec6e005b72085c1671794609176ed3bb5b0ad77
Nobuyuki Umetani <n.umetani@gmail.com>
2018-08-29 17:02:31 +0900

    move images to docs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='delfem2',
       version='0.0.0',
-      description='a codebase for geometry processing and finite element method simulation',
+      description='Handy toolset for implementing geometry processing and finite element simulation',
       author='Nobuyuki Umetani',
       author_email='n.umetani@gmail.com',
       url='https://delfem2.readthedocs.io/')
