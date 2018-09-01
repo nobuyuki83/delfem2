67cb3ef3899cb6001416a9c8258342ba9b4e7de9
Nobuyuki Umetani <n.umetani@gmail.com>
2018-09-02 07:11:29 +0900

    put Cad2D into the separate cpp/h file

--- examples_cpp/projects/cad2d/main.cpp
+++ examples_cpp/projects/cad2d/main.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  rigid_joint
-//
-//  Created by Nobuyuki Umetani on 11/3/13.
-//  Copyright (c) 2013 Nobuyuki Umetani. All rights reserved.
-//
-
 #include <iostream>
 #include <sstream>
 #include <math.h>
@@ -20,9 +12,8 @@
 #include <GL/glut.h>
 #endif
 
-#include "delfem2/mat3.h"
-
 #include "delfem2/dyntri_v3.h"
+#include "delfem2/cad2d.h"
 
 #include "delfem2/funcs_glut.h"
 #include "delfem2/funcs_gl.h"
@@ -33,175 +24,6 @@
 #endif
 
 
-class CCadTopo
-{
-public:
-  void Clear(){
-    nVertex = 0;
-    aEdge.clear();
-    aFace.clear();
-  }
-  void AddPolygon(int np){
-    const int iv0 = nVertex;
-    nVertex += np;
-    const int ie0 = aEdge.size();
-    for(int iie=0;iie<np;++iie){
-      CEdge edge0;
-      edge0.iv0 = iv0 + (iie+0)%np;
-      edge0.iv1 = iv0 + (iie+1)%np;
-      aEdge.push_back(edge0);
-    }
-    CFace face0;
-    for(int iie=0;iie<np;++iie){
-      face0.aIE.push_back( std::make_pair(ie0+iie,true ) );
-    }
-    aFace.push_back(face0);
-  }
-public:
-  class CEdge{
-  public:
-    int iv0,iv1;
-  };
-  class CFace{
-  public:
-    std::vector< std::pair<int,bool> > aIE; // index of edge, is this edge ccw?
-  };
-public:
-  int nVertex;
-  std::vector<CEdge> aEdge;
-  std::vector<CFace> aFace;
-};
-
-
-//////////////////
-
-class CCad2D
-{
-public:
-  void Clear(){
-    aVtx.clear();
-    aEdge.clear();
-    aFace.clear();
-    aTopo.Clear();
-  }
-  void Initialize_Square(){
-    Clear();
-    aTopo.AddPolygon(4);
-    aVtx.push_back(CVector2(-1.0, -1.0));
-    aVtx.push_back(CVector2(+1.0, -1.0));
-    aVtx.push_back(CVector2(+1.0, +1.0));
-    aVtx.push_back(CVector2(-1.0, +1.0));
-    int iedge0 = aEdge.size();
-    aEdge.push_back(CEdgeGeo());
-    aEdge.push_back(CEdgeGeo());
-    aEdge.push_back(CEdgeGeo());
-    aEdge.push_back(CEdgeGeo());
-    int iface0 = aFace.size();
-    aFace.push_back(CFaceGeo());
-    ////
-    aEdge[iedge0+0].GenMesh(iedge0+0,aTopo,aVtx);
-    aEdge[iedge0+1].GenMesh(iedge0+1,aTopo,aVtx);
-    aEdge[iedge0+2].GenMesh(iedge0+2,aTopo,aVtx);
-    aEdge[iedge0+3].GenMesh(iedge0+3,aTopo,aVtx);
-    aFace[iface0].GenMesh(aTopo,iface0,aEdge);
-  }
-  void Draw() const {
-    ::glDisable(GL_LIGHTING);
-    ::glColor3d(1,0,0);
-    ::glPointSize(6);
-    ::glBegin(GL_POINTS);
-    for(int iv=0;iv<aVtx.size();++iv){
-      ::glVertex3d( aVtx[iv].pos.x, aVtx[iv].pos.y, 0.0);
-    }
-    ::glEnd();
-    /////
-    ::glColor3d(0,0,0);
-    ::glLineWidth(3);
-    ::glBegin(GL_LINES);
-    for(int ie=0;ie<aEdge.size();++ie){
-      int iv0 = aTopo.aEdge[ie].iv0;
-      int iv1 = aTopo.aEdge[ie].iv1;
-      ::glVertex3d( aVtx[iv0].pos.x, aVtx[iv0].pos.y, -0.1);
-      ::glVertex3d( aVtx[iv1].pos.x, aVtx[iv1].pos.y, -0.1);
-    }
-    ::glEnd();
-    //////
-    ::glColor3d(0.8,0.8,0.8);
-    ::glLineWidth(1);
-    glTranslated(0,0,-0.2);
-    for(int iface=0;iface<aFace.size();++iface){
-      const CFaceGeo& face = aFace[iface];
-      DrawMeshTri2D_Face(face.aTri, face.aXY);
-      DrawMeshTri2D_Edge(face.aTri, face.aXY);
-    }
-    glTranslated(0,0,+0.2);
-  }
-public:
-public:
-  class CVertexGeo{
-  public:
-    CVertexGeo(const CVector2& p) : pos(p){}
-  public:
-    CVector2 pos;
-  };
-  class CEdgeGeo{
-  public:
-    void GenMesh(int iedge, const CCadTopo& topo,
-                 std::vector<CVertexGeo>& aVtxGeo)
-    {
-      assert( iedge>=0 && iedge<topo.aEdge.size() );
-      const int iv0 = topo.aEdge[iedge].iv0;
-      const int iv1 = topo.aEdge[iedge].iv1;
-      this->p0 = aVtxGeo[iv0].pos;
-      this->p1 = aVtxGeo[iv1].pos;
-    }
-  public:
-    CVector2 p0,p1;
-    std::vector<CVector2> aP;
-  };
-  class CFaceGeo{
-  public:
-//    std::vector<CVector2> aP;
-    std::vector<int> aTri;
-    std::vector<double> aXY;
-  public:
-    void GenMesh(const CCadTopo& topo,int iface0,
-                 std::vector<CEdgeGeo>& aEdgeGeo){
-      assert( iface0>=0 && iface0<topo.aFace.size() );
-      const std::vector< std::pair<int,bool> >& aIE = topo.aFace[iface0].aIE;
-      std::vector<double> aXY_corner;
-      for(int iie=0;iie<aIE.size();++iie){
-        int ie0 = aIE[iie].first;
-        assert( ie0>=0 && ie0<topo.aEdge.size() );
-        const bool dir0 = aIE[iie].second;
-        int iv0 = (dir0) ? topo.aEdge[ie0].iv0 : topo.aEdge[ie0].iv1;
-        {
-          const CEdgeGeo& eg0 = aEdgeGeo[ie0];
-          CVector2 p0 = (dir0) ? eg0.p0 : eg0.p1;
-          aXY_corner.push_back(p0.x);
-          aXY_corner.push_back(p0.y);
-        }
-      }
-      for(int ixy=0;ixy<aXY_corner.size()/2;++ixy){
-        std::cout << aXY_corner[ixy*2+0] << " " << aXY_corner[ixy*2+1] << std::endl;
-      }
-      {
-        std::vector<int> aPtrVtxInd,aVtxInd;
-        std::vector< std::vector<double> > aVecAry0;
-        aVecAry0.push_back(aXY_corner);
-        GenerateTesselation2(aTri, aXY,  aPtrVtxInd,aVtxInd,
-                             -1, false, aVecAry0);
-      }
-      std::cout << aTri.size() << std::endl;
-    }
-  };
-public:
-  CCadTopo aTopo;
-  /////
-  std::vector<CVertexGeo> aVtx;
-  std::vector<CEdgeGeo> aEdge;
-  std::vector<CFaceGeo> aFace;
-};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
---
+++ src_cpp/include/delfem2/cad2d.h
@@ -0,0 +1,98 @@
+#ifndef CAD2D_h
+#define CAD2D_h
+
+#include "delfem2/vec2.h"
+
+#include "delfem2/funcs_gl.h"
+
+class CCadTopo
+{
+public:
+  void Clear(){
+    nVertex = 0;
+    aEdge.clear();
+    aFace.clear();
+  }
+  void AddPolygon(int np){
+    const int iv0 = nVertex;
+    nVertex += np;
+    const int ie0 = aEdge.size();
+    for(int iie=0;iie<np;++iie){
+      CEdge edge0;
+      edge0.iv0 = iv0 + (iie+0)%np;
+      edge0.iv1 = iv0 + (iie+1)%np;
+      aEdge.push_back(edge0);
+    }
+    CFace face0;
+    for(int iie=0;iie<np;++iie){
+      face0.aIE.push_back( std::make_pair(ie0+iie,true ) );
+    }
+    aFace.push_back(face0);
+  }
+public:
+  class CEdge{
+  public:
+    int iv0,iv1;
+  };
+  class CFace{
+  public:
+    std::vector< std::pair<int,bool> > aIE; // index of edge, is this edge ccw?
+  };
+public:
+  int nVertex;
+  std::vector<CEdge> aEdge;
+  std::vector<CFace> aFace;
+};
+
+
+
+class CCad2D_VtxGeo{
+public:
+  CCad2D_VtxGeo(const CVector2& p) : pos(p){}
+public:
+  CVector2 pos;
+};
+class CCad2D_EdgeGeo{
+public:
+  void GenMesh(int iedge, const CCadTopo& topo,
+               std::vector<CCad2D_VtxGeo>& aVtxGeo);
+public:
+  CVector2 p0,p1;
+  std::vector<CVector2> aP;
+};
+class CCad2D_FaceGeo{
+public:
+  //    std::vector<CVector2> aP;
+  std::vector<int> aTri;
+  std::vector<double> aXY;
+public:
+  void GenMesh(const CCadTopo& topo,int iface0,
+               std::vector<CCad2D_EdgeGeo>& aEdgeGeo);
+};
+
+//////////////////
+
+class CCad2D
+{
+public:
+  void Clear(){
+    aVtx.clear();
+    aEdge.clear();
+    aFace.clear();
+    topo.Clear();
+  }
+  void Initialize_Square();
+  void Draw() const;
+public:
+public:
+public:
+  CCadTopo topo;
+  /////
+  std::vector<CCad2D_VtxGeo> aVtx;
+  std::vector<CCad2D_EdgeGeo> aEdge;
+  std::vector<CCad2D_FaceGeo> aFace;
+};
+
+
+
+#endif
