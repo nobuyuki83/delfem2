d59ad7bb126c3f55a70a0514f4625475a3d383df
Nobuyuki Umetani <n.umetani@gmail.com>
2018-12-21 18:22:52 +0900

    py depth

--- examples_py/depth.py
+++ examples_py/depth.py
@@ -13,14 +13,14 @@ def main():
   axis.line_width=3
 
   sampler = dfm2.GPUSampler()
-  sampler.init(size_res_width=256,size_res_height=256,   is_color=True,is_depth=True)
+  sampler.init(size_res_width=256,size_res_height=256,   format_color="4byte",is_depth=True)
   sampler.set_coordinate(len_grid=0.2, depth_max=100.0,
                        org=[0, -50, 0], dir_prj=[0, -1.0, 0], dir_width=[1, 0, 0])  
   sampler.color = [0,0,1,1]
   sampler.len_axis = 10
   sampler.bgcolor = [1,1,0,1]
 
-  buffer = dfm2.DepthColorBuffer(win_size=[512,512],is_color=True,is_depth=True)
+  buffer = dfm2.DepthColorBuffer(win_size=[512,512],format_color="4byte",is_depth=True)
   dfm2.take_depth_shot(mshelm.draw,sampler,buffer)
   buffer.close()
 
