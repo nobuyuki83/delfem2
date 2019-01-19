02c78eb02a5a7e0829bdbbb3b3266bdcfbcfdd46
Nobuyuki Umetani <n.umetani@gmail.com>
2019-01-19 12:59:00 +0900

    polyline

--- module_py/py_polyline.cpp
+++ module_py/py_polyline.cpp
@@ -10,7 +10,7 @@
 
 namespace py = pybind11;
 
-int add_edge
+int getLevelSet_AddEdge
 (int ip0, int ip1,
  double v0, double v1,
  std::map<std::pair<int,int>, std::pair<int,double> >& aNode)
@@ -63,85 +63,69 @@ public:
 };
 
 
-std::vector<std::vector<int> > getLevelSet(const py::array_t<float>& a, double mag)
+void getLevelSet_GetEdges
+(std::map<std::pair<int,int>, std::pair<int,double> >& mapNode,
+std::vector< std::pair<int,int> >& aEdge,
+ int nh, int nw, float thres, const float* pVal)
 {
-  assert(a.ndim()==2);
-  const int mh = a.shape()[0];
-  const int mw = a.shape()[1];
-  const int nh = mh-1;
-  const int nw = mw-1;
-  const float* pVal = a.data();
-  double thre = 0.5;
-  std::map<std::pair<int,int>, std::pair<int,double> > mapNode;
-  std::vector< std::pair<int,int> > aEdge;
+//  const int mh = nh+1;
+  const int mw = nw+1;
   for(int ih=0;ih<nh;++ih){ // x
     for(int iw=0;iw<nw;++iw){ // y
-      int ip0 = (ih+0)*mw+(iw+0);
-      int ip1 = (ih+0)*mw+(iw+1);
-      int ip2 = (ih+1)*mw+(iw+0);
-      int ip3 = (ih+1)*mw+(iw+1);
-      float v0 = pVal[ip0]-thre;
-      float v1 = pVal[ip1]-thre;
-      float v2 = pVal[ip2]-thre;
-      float v3 = pVal[ip3]-thre;
+      const int ip0 = (ih+0)*mw+(iw+0);
+      const int ip1 = (ih+0)*mw+(iw+1);
+      const int ip2 = (ih+1)*mw+(iw+0);
+      const int ip3 = (ih+1)*mw+(iw+1);
+      const float v0 = pVal[ip0]-thres;
+      const float v1 = pVal[ip1]-thres;
+      const float v2 = pVal[ip2]-thres;
+      const float v3 = pVal[ip3]-thres;
       if( (v0>0 && v1<0 && v2<0 && v3<0) ||  (v0<0 && v1>0 && v2>0 && v3>0) ){
-        int in0 = add_edge(ip0,ip1,v0,v1,mapNode);
-        int in1 = add_edge(ip0,ip2,v0,v2,mapNode);
+        int in0 = getLevelSet_AddEdge(ip0,ip1,v0,v1,mapNode);
+        int in1 = getLevelSet_AddEdge(ip0,ip2,v0,v2,mapNode);
         aEdge.push_back(std::make_pair(in0,in1));
       }
       if( (v0<0 && v1>0 && v2<0 && v3<0) ||  (v0>0 && v1<0 && v2>0 && v3>0) ){
-        int in0 = add_edge(ip0,ip1,v0,v1,mapNode);
-        int in1 = add_edge(ip1,ip3,v1,v3,mapNode);
+        int in0 = getLevelSet_AddEdge(ip0,ip1,v0,v1,mapNode);
+        int in1 = getLevelSet_AddEdge(ip1,ip3,v1,v3,mapNode);
         aEdge.push_back(std::make_pair(in0,in1));
       }
       if( (v0<0 && v1<0 && v2>0 && v3<0) ||  (v0>0 && v1>0 && v2<0 && v3>0) ){
-        int in0 = add_edge(ip0,ip2,v0,v2,mapNode);
-        int in1 = add_edge(ip2,ip3,v2,v3,mapNode);
+        int in0 = getLevelSet_AddEdge(ip0,ip2,v0,v2,mapNode);
+        int in1 = getLevelSet_AddEdge(ip2,ip3,v2,v3,mapNode);
         aEdge.push_back(std::make_pair(in0,in1));
       }
       if( (v0<0 && v1<0 && v2<0 && v3>0) ||  (v0>0 && v1>0 && v2>0 && v3<0) ){
-        int in0 = add_edge(ip1,ip3,v1,v3,mapNode);
-        int in1 = add_edge(ip2,ip3,v2,v3,mapNode);
+        int in0 = getLevelSet_AddEdge(ip1,ip3,v1,v3,mapNode);
+        int in1 = getLevelSet_AddEdge(ip2,ip3,v2,v3,mapNode);
         aEdge.push_back(std::make_pair(in0,in1));
       }
       if( (v0>0 && v1>0 && v2<0 && v3<0) ||  (v0<0 && v1<0 && v2>0 && v3>0) ){
-        int in0 = add_edge(ip0,ip2,v0,v2,mapNode);
-        int in1 = add_edge(ip1,ip3,v1,v3,mapNode);
+        int in0 = getLevelSet_AddEdge(ip0,ip2,v0,v2,mapNode);
+        int in1 = getLevelSet_AddEdge(ip1,ip3,v1,v3,mapNode);
         aEdge.push_back(std::make_pair(in0,in1));
       }
       if( (v0>0 && v1<0 && v2>0 && v3<0) ||  (v0<0 && v1>0 && v2<0 && v3>0) ){
-        int in0 = add_edge(ip0,ip1,v0,v1,mapNode);
-        int in1 = add_edge(ip2,ip3,v2,v3,mapNode);
+        int in0 = getLevelSet_AddEdge(ip0,ip1,v0,v1,mapNode);
+        int in1 = getLevelSet_AddEdge(ip2,ip3,v2,v3,mapNode);
         aEdge.push_back(std::make_pair(in0,in1));
       }
       if( (v0>0 && v1<0 && v2<0 && v3>0) ||  (v0<0 && v1>0 && v2>0 && v3<0) ){
-        int in0 = add_edge(ip0,ip1,v0,v1,mapNode);
-        int in1 = add_edge(ip0,ip2,v0,v2,mapNode);
+        int in0 = getLevelSet_AddEdge(ip0,ip1,v0,v1,mapNode);
+        int in1 = getLevelSet_AddEdge(ip0,ip2,v0,v2,mapNode);
         aEdge.push_back(std::make_pair(in0,in1));
-        int in2 = add_edge(ip1,ip3,v1,v3,mapNode);
-        int in3 = add_edge(ip2,ip3,v2,v3,mapNode);
+        int in2 = getLevelSet_AddEdge(ip1,ip3,v1,v3,mapNode);
+        int in3 = getLevelSet_AddEdge(ip2,ip3,v2,v3,mapNode);
         aEdge.push_back(std::make_pair(in2,in3));
       }
     }
   }
-  const int nNode = mapNode.size();
-  std::vector<CLevSetNode> aNode(nNode);
-  for(auto itr=mapNode.begin();itr!=mapNode.end();++itr){
-    CLevSetNode node;
-    node.ip0 = itr->first.first;
-    node.ip1 = itr->first.second;
-    node.ratio = itr->second.second;
-    int in0 = itr->second.first;
-    aNode[in0] = node;
-  }
-  for(unsigned int ie=0;ie<aEdge.size();++ie){
-    int in0 = aEdge[ie].first;
-    int in1 = aEdge[ie].second;
-    aNode[in0].setNeighbour(in1);
-    aNode[in1].setNeighbour(in0);
-  }
-  ///////////////////////////////////
-  std::vector< std::deque<int> > aLoop;
+}
+
+void getLevelSet_MakeLoop
+(std::vector< std::deque<int> >& aLoop,
+ const std::vector<CLevSetNode>& aNode)
+{
   unsigned int ino_ker = 0;
   for(;;){
     for(;ino_ker<aNode.size();++ino_ker){
@@ -191,19 +175,86 @@ std::vector<std::vector<int> > getLevelSet(const py::array_t<float>& a, double m
       }
     }
   }
+}
+
+std::vector<std::vector<double> > getLevelSet
+(int mh, int mw,
+ const float* pVal, double thres)
+{
+  const int nh = mh-1;
+  const int nw = mw-1;
+  std::vector<CLevSetNode> aNode;
+  {
+    std::vector< std::pair<int,int> > aEdge;
+    std::map<std::pair<int,int>, std::pair<int,double> > mapNode;
+    getLevelSet_GetEdges(mapNode,aEdge,
+                         nh,nw,thres,pVal);
+    const int nNode = mapNode.size();
+    aNode.resize(nNode);
+    for(auto itr=mapNode.begin();itr!=mapNode.end();++itr){
+      CLevSetNode node;
+      node.ip0 = itr->first.first;
+      node.ip1 = itr->first.second;
+      node.ratio = itr->second.second;
+      int in0 = itr->second.first;
+      aNode[in0] = node;
+    }
+    for(unsigned int ie=0;ie<aEdge.size();++ie){
+      const int in0 = aEdge[ie].first;
+      const int in1 = aEdge[ie].second;
+      aNode[in0].setNeighbour(in1);
+      aNode[in1].setNeighbour(in0);
+    }
+  }
+  ///////////////////////////////////
+  std::vector< std::deque<int> > aLoop;
+  getLevelSet_MakeLoop(aLoop,aNode);
   /////////
-  std::vector< std::vector<int> > aRes;
+  std::vector< std::vector<double> > aRes;
   aRes.resize(aLoop.size());
   for(unsigned int iloop=0;iloop<aLoop.size();++iloop){
     for(unsigned int iin=0;iin<aLoop[iloop].size();++iin){
       int in0 = aLoop[iloop][iin];
       assert( in0>=0 && in0<(int)aNode.size() );
       double x0, y0; aNode[in0].pos(x0,y0,mw);
-      aRes[iloop].push_back(int(x0*mag));
-      aRes[iloop].push_back(int(y0*mag));
+      aRes[iloop].push_back(x0);
+      aRes[iloop].push_back(y0);
     }
   }
   return aRes;
+}
+
+
+std::vector<std::vector<int> > pyGetLevelSet(const py::array_t<float>& a, double mag)
+{
+  assert(a.ndim()==2);
+  const int mh0 = a.shape()[0];
+  const int mw0 = a.shape()[1];
+  const int mh1 = mh0+2;
+  const int mw1 = mw0+2;
+  std::vector<float> a1(mh1*mw1,0.0);
+  for(int ih=0;ih<mh0;++ih){
+  for(int iw=0;iw<mw0;++iw){
+    const int ip0 = (ih+0)*mw0+(iw+0);
+    const int ip1 = (ih+1)*mw1+(iw+1);
+    a1[ip1] = a.data()[ip0];
+  }
+  }
+  std::vector<std::vector<double> > aLoop = getLevelSet(mh1, mw1, a1.data(), 0.5);
+  std::vector<std::vector<int> > aLoopInt(aLoop.size());
+  for(unsigned int iloop=0;iloop<aLoop.size();++iloop){
+    std::vector<double> loop = aLoop[iloop];
+    const int np = loop.size()/2;
+    for(int ip=0;ip<np;++ip){
+      double x0 = loop[ip*2+0];
+      double y0 = loop[ip*2+1];
+      double ix0 = int((x0+0.5-1.0)*mag);
+      double iy0 = int((y0+0.5-1.0)*mag);
+      aLoopInt[iloop].push_back(ix0);
+      aLoopInt[iloop].push_back(iy0);
+    }
+  }
+  return aLoopInt;
 }
 
 
@@ -268,6 +319,6 @@ std::vector<int> simplifyPolyloop(std::vector<int>& aXY_in, double eps)
 
 
 void init_polyline(py::module &m){
-  m.def("get_level_set", getLevelSet);
+  m.def("get_level_set", pyGetLevelSet);
   m.def("simplify_polyloop",simplifyPolyloop);
 }
