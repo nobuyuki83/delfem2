2e4f8dd16a200e187cfce2975ef11872ebf58a4e
Nobuyuki Umetani <n.umetani@gmail.com>
2019-03-03 13:16:39 +0900

    add texture

--- module_py/py_delfem2.cpp
+++ module_py/py_delfem2.cpp
@@ -53,6 +53,7 @@ void init_polyline(py::module &m);
 void init_mshtopoio_gl(py::module &m);
 void init_sampler(py::module &m);
 void init_fbx(py::module &m);
+void init_texture(py::module &m);
 
 
 PYBIND11_MODULE(dfm2, m) {
@@ -67,6 +68,8 @@ PYBIND11_MODULE(dfm2, m) {
   init_mshtopoio_gl(m);
   init_sampler(m);
   init_polyline(m);
+  init_texture(m);
+  
   
   ///////////////////////////////////
   // axis arrigned boudning box
