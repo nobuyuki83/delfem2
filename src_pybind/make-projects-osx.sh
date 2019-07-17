1b7cad902b5d24a0dcd345b8b5cbddf49c3de0c5
Nobuyuki Umetani <n.umetani@gmail.com>
2019-07-17 09:00:42 +0900

    ci

--- src_pybind/make-projects-osx.sh
+++ src_pybind/make-projects-osx.sh
@@ -2,13 +2,16 @@ cd core
 mkdir buildXcode 
 cd buildXcode
 cmake -G Xcode -DPYTHON_EXECUTABLE:PATH=../../../../myenv/bin/python3  ..
+cd ../../
 
 cd gl
 mkdir buildXcode 
 cd buildXcode
 cmake -G Xcode -DPYTHON_EXECUTABLE:PATH=../../../../myenv/bin/python3  ..
+cd ../../
 
 cd eigen
 mkdir buildXcode 
 cd buildXcode
 cmake -G Xcode -DPYTHON_EXECUTABLE:PATH=../../../../myenv/bin/python3  ..
+cd ../../
