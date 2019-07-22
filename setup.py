2e66434e4c825ddf75588ead2372fd03840fa8d6
Nobuyuki Umetani <n.umetani@gmail.com>
2019-07-23 00:02:30 +0900

    linux install sucess from PyPI

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ from setuptools.command.build_ext import build_ext
 
 
 class CMakeExtension(Extension):
-  def __init__(self, name, sourcedir=''):
+  def __init__(self, name, sourcedir='',distdir=''):
     Extension.__init__(self, name, sources=[])
     self.sourcedir = os.path.abspath(sourcedir)
-
+    self.distdir = distdir
 
 class CMakeBuild(build_ext):
   def run(self):
@@ -35,6 +35,7 @@ class CMakeBuild(build_ext):
     for ext in self.extensions:
       pathdir,name0 = os.path.split(pathtmp)
       self.build_temp = os.path.join(pathdir,ext.name+"_"+name0)
+      print(type(ext))
       self.build_extension(ext)
 
   def build_extension(self, ext):
@@ -70,29 +71,33 @@ class CMakeBuild(build_ext):
                           cwd=self.build_temp, env=env)
     subprocess.check_call(['cmake', '--build', '.'] + build_args,
                           cwd=self.build_temp)
-    #subprocess.run(["rm",'-r',self.build_temp])
+    path_root = os.path.dirname(os.path.abspath(__file__))
+    path_from = os.path.join(os.path.join(path_root,ext.distdir),ext.name)+".*"
+    path_to = os.path.join(os.path.join(extdir,ext.distdir),"")
+    print("path_from",path_from)
+    print("path_to",path_to)    
+    if os.path.dirname(path_from) != os.path.dirname(path_to):
+      subprocess.check_call('cp '+path_from+" "+path_to,shell=True)
     print()  # Add an empty line for cleaner output
 
 
 setup(name='PyDelFEM2',
-      version='0.0.6',
+      version='0.0.8',
       description='a framework for geometry processing and finite element analysis',
       author='Nobuyuki Umetani',
       author_email='n.umetani@gmail.com',
       url='https://github.com/nobuyuki83/delfem2',
-      packages=['PyDelFEM2'],
       classifiers=['Topic :: Scientific/Engineering :: Physics',
                    'Topic :: Games/Entertainment :: Simulation',
                    'License :: OSI Approved :: MIT License'],
       keywords=['fem','simulation','mesh','computer graphics'],
       install_requires=[ 'numpy', 'PyOpenGL', 'glfw', 'PySide2' ],
       license="MIT",
-      py_modules = ['PyDelFEM2','PyDelFEM2.gl','PyDelFEM2.eigen','PyDelFEM2.qt' ],      
-      package_data={'PyDelFEM2':['*.so','gl/*.so','gl/*.py','eigen/*.so','eigen/*.py','qt/*.py']},
       zip_safe=False,      
-      ext_modules=[ CMakeExtension('c_core','src_pybind/core'),
-                    CMakeExtension('c_gl','src_pybind/gl'),
-                    CMakeExtension('c_eigen','src_pybind/eigen'),
+      packages=['PyDelFEM2','PyDelFEM2.gl','PyDelFEM2.eigen','PyDelFEM2.qt'],      
+      ext_modules=[ CMakeExtension('c_core', 'src_pybind/core', 'PyDelFEM2'),
+                    CMakeExtension('c_gl',   'src_pybind/gl',   'PyDelFEM2/gl'),
+                    CMakeExtension('c_eigen','src_pybind/eigen','PyDelFEM2/eigen'),
                     ],  # location of *.so file, cmake file
       cmdclass=dict(build_ext=CMakeBuild),
       )
