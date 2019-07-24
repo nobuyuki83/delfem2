f401f93dbf0373c4f622cb5405ec4337c4f622ad
Nobuyuki Umetani <n.umetani@gmail.com>
2019-07-24 21:32:08 +0900

    PyOpenGL camera rotate mode ytop

--- PyDelFEM2/gl/_gl.py
+++ PyDelFEM2/gl/_gl.py
@@ -8,13 +8,13 @@
 
 import math, numpy
 import OpenGL.GL as gl
-import sys
-sys.path.append("..")
 
 from ..util import minMaxLoc, v3_scale, v3_normalize, v3_cross
 from ..util import motion_rot, motion_trans
 from ..util import get_quaternion_rot_matrix, affine_matrix_quaternion
 from ..util import screenUnProjection, screenUnProjectionDirection
+
+from enum import Enum
 
 def getOpenglInfo():
   info = """
@@ -93,6 +93,44 @@ def draw_pyramid(lenWh, lenHh, lenZ, Rot, trans):
   glVertex3dv(pos4)
   glEnd()
 
+
+'''
+def glhLookAtf2(matrix:List[float], eyePosition3D:List[float], 
+                  float *center3D, float *upVector3D )
+{
+   float forward[3], side[3], up[3];
+   float matrix2[16], resultMatrix[16];
+   // --------------------
+   forward[0] = center3D[0] - eyePosition3D[0];
+   forward[1] = center3D[1] - eyePosition3D[1];
+   forward[2] = center3D[2] - eyePosition3D[2];
+   NormalizeVector(forward);
+   // --------------------
+   // Side = forward x up
+   ComputeNormalOfPlane(side, forward, upVector3D);
+   NormalizeVector(side);
+   --------------------
+   // Recompute up as: up = side x forward
+   ComputeNormalOfPlane(up, side, forward);
+   // --------------------
+   matrix2[0] = side[0];
+   matrix2[4] = side[1];
+   matrix2[8] = side[2];
+   matrix2[12] = 0.0;
+   // --------------------
+   matrix2[1] = up[0];
+   matrix2[5] = up[1];
+   matrix2[9] = up[2];
+   matrix2[13] = 0.0;
+   // --------------------
+'''
+
+class CAMERA_ROT_MODE(Enum):
+  YTOP = 1
+  ZTOP = 2
+  TBALL = 3
+
+
 class Camera:
   def __init__(self, view_height=1.0):
     self.view_height = view_height
@@ -100,7 +138,10 @@ class Camera:
     self.scr_trans = [0., 0.] # position of the pivot in the screen
     self.pivot = [0., 0., 0.] # pivot location
     self.quat = [1, 0, 0, 0]
+    self.camera_rot_mode = CAMERA_ROT_MODE.TBALL
     self.fovy = 60  # degree
+    self.theta = 0.0
+    self.psi = 0.0
 
   def set_gl_camera(self):
     viewport = gl.glGetIntegerv(gl.GL_VIEWPORT)
@@ -122,8 +163,24 @@ class Camera:
     gl.glMatrixMode(gl.GL_MODELVIEW)
     gl.glLoadIdentity()
     gl.glTranslated(self.scr_trans[0], self.scr_trans[1], -depth)
-    Rview = affine_matrix_quaternion(self.quat)
-    gl.glMultMatrixd(Rview)
+    if self.camera_rot_mode == CAMERA_ROT_MODE.TBALL:
+      Rview = affine_matrix_quaternion(self.quat)
+      gl.glMultMatrixd(Rview)
+    elif self.camera_rot_mode == CAMERA_ROT_MODE.YTOP:
+      x = math.sin(self.theta)*math.cos(self.psi)
+      z = math.cos(self.theta)*math.cos(self.psi)
+      y = math.sin(self.psi)
+      ey = numpy.array([x,y,z])
+      up = numpy.array([0,1,0])
+      up =  up-ey*numpy.dot(ey,up)
+      up /= numpy.linalg.norm(up)
+      vx = v3_normalize(v3_cross(up, ey))
+      mMV = [vx[0], up[0], ey[0], 0,
+             vx[1], up[1], ey[1], 0,
+             vx[2], up[2], ey[2], 0,
+             0,     0,     0,     1]
+      mMV = numpy.array(mMV)
+      gl.glMultMatrixf(mMV)
     gl.glTranslated(self.pivot[0], self.pivot[1], self.pivot[2])
 
   def set_rotation(self, camera_eye_up):
@@ -136,9 +193,14 @@ class Camera:
     self.quat = get_quaternion_rot_matrix(mat)
 
   def rotation(self,sx1,sy1,sx0,sy0):
-    self.quat, self.trans = motion_rot(
-      sx1, sy1, sx0, sy0,
-      self.quat, self.scr_trans)
+    if self.camera_rot_mode == CAMERA_ROT_MODE.TBALL:
+      self.quat, self.trans = motion_rot(
+        sx1, sy1, sx0, sy0,
+        self.quat, self.scr_trans)
+    elif self.camera_rot_mode == CAMERA_ROT_MODE.YTOP:
+      self.theta += sx1-sx0
+      self.psi   += sy1-sy0
+      self.psi = min(max(-math.pi*0.499,self.psi),math.pi*0.499)
     return
 
   def translation(self,sx1,sy1,sx0,sy0):
