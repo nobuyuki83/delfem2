12f3a57205b15b6d086b5827f074ba556757ccf7
Nobuyuki Umetani <n.umetani@gmail.com>
2019-11-07 00:59:01 +0900

    moved fem examples from glut to glfw #2

--- examples_glfw_oldgl/600_Fem2DHelmholtz/main.cpp
+++ examples_glfw_oldgl/600_Fem2DHelmholtz/main.cpp
@@ -15,22 +15,15 @@
 #include "delfem2/cad2d.h"
 
 // ---------------
-
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
-
+#include <GLFW/glfw3.h>
+#include "delfem2/opengl/glfw_viewer.hpp"
 #include "delfem2/opengl/gl2_color.h"
 #include "delfem2/opengl/gl2_funcs.h"
-#include "../glut_cam.h"
 
 namespace dfm2 = delfem2;
 
 // --------------------------------------------
 
-CNav3D_GLUT nav;
 std::vector<unsigned int> aTri1;
 std::vector<double> aXY1;
 std::vector<int> aBCFlag; // boundary condition flag
@@ -39,16 +32,13 @@ int ipCenter = -1;
 
 std::vector<std::complex<double> > aCVal;
 std::vector<double> aVal;
-double time_cur  = 0.0;
 
 CMatrixSparse<std::complex<double> > mat_A;
 std::vector<std::complex<double> > vec_b;
 CPreconditionerILU<std::complex<double> > ilu_A;
 
-bool is_animation = false;
 
-
-//////////////////////////////////////////////////////////////////////////////////////
+// ---------------------------------------------------
 
 void MakeMesh(){
   CCad2D cad2d;
@@ -73,8 +63,6 @@ void MakeMesh(){
   ipCenter = 4;
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// iproblem: 0, 1
 void InitializeProblem_Scalar()
 {
   const int np = (int)aXY1.size()/2;
@@ -82,12 +70,12 @@ void InitializeProblem_Scalar()
   aBCFlag.resize(np,0);
   aBCFlag[ipCenter] = 1;
   aCVal[ipCenter] = 1;
-  //////
+  //
   std::vector<int> psup_ind, psup;
   JArrayPointSurPoint_MeshOneRingNeighborhood(psup_ind, psup,
                                               aTri1.data(), aTri1.size()/3, 3, (int)aXY1.size()/2);
   JArray_Sort(psup_ind, psup);
-  ////
+  //
   mat_A.Initialize(np, 1, true);
   mat_A.SetPattern(psup_ind.data(), psup_ind.size(), psup.data(),psup.size());
   ilu_A.Initialize_ILU0(mat_A);
@@ -117,11 +105,10 @@ void SolveProblem_Poisson()
                                                        aaIP[ipl].data(),aaIP[ipl].size(),
                                                        aCVal.data());
   }
-
-
+  
   mat_A.SetBoundaryCondition(aBCFlag.data(),aBCFlag.size(),1);
   setRHS_Zero(vec_b, aBCFlag,0);
-  ///////////////////////////
+  // ----------------
   std::vector<std::complex<double> > vec_x;
   ilu_A.SetValueILU(mat_A);
   ilu_A.DoILUDecomp();
@@ -143,32 +130,16 @@ void SolveProblem_Poisson()
   }
 
 //  SolveLinSys_PCG(mat_A,vec_b,vec_x,ilu_A, conv_ratio,iteration);
-  ///////////////////////////
+  //
   XPlusAY(aCVal,
           nDoF,aBCFlag, std::complex<double>(1.0),vec_x);
 }
 
 
-//////////////////////////////////////////////////////////////////////////////////////
-
-void myGlutResize(int w, int h)
-{
-  glViewport(0, 0, w, h);
-  glutPostRedisplay();
-}
+// -----------------------------------------------------
 
 void myGlutDisplay(void)
 {
-  //  ::glClearColor(0.2, .7, 0.7, 1.0);
-  ::glClearColor(1.0, 1.0, 1.0, 1.0);
-  ::glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-  ::glEnable(GL_DEPTH_TEST);
-  
-  ::glEnable(GL_POLYGON_OFFSET_FILL );
-  ::glPolygonOffset( 1.1, 4.0 );
-  
-  nav.SetGL_Camera();
-  
   delfem2::opengl::DrawMeshTri2D_Edge(aTri1,aXY1);
   ::glPointSize(2);
   ::glColor3d(0,0,0);
@@ -180,84 +151,36 @@ void myGlutDisplay(void)
   delfem2::opengl::DrawMeshTri2D_ScalarP1(aXY1.data(),aXY1.size()/2,
                                  aTri1.data(),aTri1.size()/3,
                                  aVal.data(),1,colorMap);
-  
-  ::glColor3d(0,0,0);
-  ShowFPS();
-  
-  glutSwapBuffers();
-}
-
-void myGlutIdle(){
-  if( is_animation ){
-    time_cur += 0.3;
-    std::complex<double> rot(cos(time_cur),sin(time_cur));
-    for(int ip=0;ip<aVal.size();++ip){
-      aVal[ip] = (rot*aCVal[ip]).real();
-    }
-  }
-  ::glutPostRedisplay();
-}
-
-void myGlutMotion( int x, int y ){
-  nav.glutMotion(x,y);
-}
-
-void myGlutMouse(int button, int state, int x, int y){
-    nav.glutMouse(button,state,x,y);
-}
-
-void myGlutKeyboard(unsigned char key, int x, int y)
-{
-  switch (key) {
-    case 'q':
-    case 'Q':
-    case '\033':  /* '\033' ÇÕ ESC ÇÃ ASCII ÉRÅ[Éh */
-      exit(0);
-      break;
-    case 'a':
-      is_animation = !is_animation; 
-      break;
-    default:
-      break;
-  }
-}
-
-void myGlutSpecial(int key, int x, int y){
-  nav.glutSpecial(key,x,y);
-  switch(key){
-  }
-  ::myGlutResize(-1,-1);
-  ::glutPostRedisplay();
 }
 
 int main(int argc,char* argv[])
 {
-  // Initailze GLUT
-  ::glutInitWindowPosition(200,200);
-  ::glutInitWindowSize(1000, 500);
-  ::glutInit(&argc, argv);
-  ::glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
-  ::glutCreateWindow("Cad View");
-  
-  // Set callback function
-  ::glutMotionFunc(myGlutMotion);
-  ::glutMouseFunc(myGlutMouse);
-  ::glutDisplayFunc(myGlutDisplay);
-  ::glutReshapeFunc(myGlutResize);
-  ::glutKeyboardFunc(myGlutKeyboard);
-  ::glutSpecialFunc(myGlutSpecial);
-  ::glutIdleFunc(myGlutIdle);
-  
   MakeMesh();
   InitializeProblem_Scalar();
   SolveProblem_Poisson();
-  
   aVal.resize(aCVal.size(),0.1);
-  time_cur = 0.0;
   for(int ip=0;ip<aVal.size();++ip){ aVal[ip] = aCVal[ip].real(); }
   
-  // Enter main loop
-  nav.camera.view_height = 2.0;
-  ::glutMainLoop();
+  delfem2::opengl::CViewer_GLFW viewer;
+  viewer.nav.camera.view_height = 1.5;
+  viewer.Init_oldGL();
+  while(!glfwWindowShouldClose(viewer.window)){
+    {
+      static int iframe = 0;
+      double time_cur = iframe*0.01;
+      std::complex<double> rot(cos(time_cur),sin(time_cur));
+      for(int ip=0;ip<aVal.size();++ip){
+        aVal[ip] = (rot*aCVal[ip]).real();
+      }
+      iframe++;
+    }
+      // ------------
+    viewer.DrawBegin_oldGL();
+    myGlutDisplay();
+    viewer.DrawEnd_oldGL();
+  }
+  glfwDestroyWindow(viewer.window);
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
   return 0;
 }
