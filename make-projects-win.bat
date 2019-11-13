24d06c64ac638202d0ae23f3522c1cf8b88cc09f
Nobuyuki <n.umetani@gmail.com>
2019-11-13 14:38:08 +0900

    windows batch file updates

--- make-projects-win.bat
+++ make-projects-win.bat
@@ -1,26 +1,52 @@
-
-
-
-
 git submodule update --init --recursive
+
+: ################################
+: test cpp
+
+cd test_cpp/googletest
+mkdir buildVS64
+cd buildVS32
+cmake -A x64 .. 
+cmake --build . --config Release
+cd ../../..
+
+cd test_cpp
+mkdir buildVS64
+cd buildVS64
+cmake -A x64 ..
+cmake --build . --config Release
+Release/runUnitTests.exe
+cd ../../
+
+cd test_cpp
+mkdir buildVS32
+cd buildVS32
+cmake -A Win32 ..
+cmake --build . --config Release
+Release/runUnitTests.exe
+cd ../../
+
+: ################################
+: glfw
+
 cd src_cpp/external/glfw
 mkdir buildVS64
 cd buildVS64
 cmake -A x64 ..
 cmake --build . --config Release
-:: cmake --build . --config Debug
+rem cmake --build . --config Debug
 cd ..
 mkdir buildVS32
 cd buildVS32
 cmake -A Win32 ..
 cmake --build . --config Release
-:: cmake --build . --config Debug
+rem cmake --build . --config Debug
 cd ../
 cd ../../../
 
 
-
-
+: ###############################
+: pybind
 
 cd src_pybind/core
 mkdir buildVS64
@@ -32,52 +58,39 @@ cd ../../../
 cd src_pybind/gl
 mkdir buildVS64
 cd buildVS64
-cmake -A x64 -DCMAKE_PREFIX_PATH="C:/Program Files/glew" ..
+cmake -A x64 ..
 cmake --build . --config Release
 cd ../../../
 
 
-cd src_pybind/eigen
-mkdir buildVS64
-cd buildVS64
-cmake -A x64 -DCMAKE_PREFIX_PATH="C:/Program Files/glew" ..
-cmake --build . --config Release
-cd ../../../
+: ##############################
+: glfw_oldgl
 
-
-cd examples_cpp
+cd examples_glfw_oldgl
 mkdir buildVS32
 cd buildVS32
-cmake -A Win32 -DCMAKE_PREFIX_PATH="C:/Program Files (x86)/glew;C:/Program Files (x86)/freeglut" ..
-cmake --build .
-cd ../../
-
-
-cd examples_cpp
-mkdir buildVS64
-cd buildVS64
-cmake -A x64 -DCMAKE_PREFIX_PATH="C:/Program Files/glew;C:/Program Files/freeglut" ..
-cmake --build .
-cd ../../
-
-
-
-pip uninstall PyDelFEM2 -y
-pip uninstall PyDelFEM2 -y
-python setup.py install
-python setup.py test
-
-
-cd tests\googletest
-mkdir buildVS64
-cd buildVS64
-cmake -A x64 .. 
+cmake -A Win32 ..
 cmake --build . --config Release
-cd ..\..\..
-cd tests
+cd ../../
+
+cd examples_glfw_oldgl
 mkdir buildVS64
 cd buildVS64
 cmake -A x64 ..
 cmake --build . --config Release
-Release\runUnitTests.exe
-cd ..\..\
+cd ../../
+
+: cd examples_cpp
+: mkdir buildVS64
+: cd buildVS64
+: cmake -A x64 -DCMAKE_PREFIX_PATH="C:/Program Files/glew;C:/Program Files/freeglut" ..
+: cmake --build .
+: cd ../../
+
+
+: pip uninstall PyDelFEM2 -y
+: pip uninstall PyDelFEM2 -y
+: python setup.py install
+: python setup.py test
+
+
