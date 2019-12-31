c1d2dd82af380c205607e79220341e2d73282683
Nobuyuki Umetani <n.umetani@gmail.com>
2020-01-01 00:37:51 +0900

    make laplace deform example better

--- examples_glfw_oldgl/400_LaplaceMeshDeform/main.cpp
+++ examples_glfw_oldgl/400_LaplaceMeshDeform/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <cmath>
-#include <ctime>
-#include "delfem2/primitive.h"
 #include "delfem2/mshtopo.h"
 #include "delfem2/mats.h"
 #include "delfem2/emat.h"
@@ -90,7 +88,7 @@ void SetLinSys_LaplaceGraph_MeshTri3D(
 // ------------------------------------
 
 std::vector<unsigned int> aTri;
-std::vector<double> aXYZ;
+std::vector<double> aXYZ0;
 std::vector<double> aXYZ1;
 std::vector<double> aDelta;
 
@@ -117,49 +115,54 @@ void SetProblem()
                                  */
   {
     dfm2::Read_Obj(std::string(PATH_INPUT_DIR)+"/bunny_1k.obj",
-        aXYZ,aTri);
-    dfm2::Normalize_Points3D(aXYZ,1.0);
-    dfm2::Rotate_Points3D(aXYZ, -M_PI*0.5, 0.0, 0.0);
+        aXYZ0,aTri);
+    dfm2::Normalize_Points3D(aXYZ0,1.0);
+    dfm2::Rotate_Points3D(aXYZ0, -M_PI*0.5, 0.0, 0.0);
   }
 
   {
     std::vector<unsigned int> psup_ind, psup;
     JArrayPointSurPoint_MeshOneRingNeighborhood(psup_ind, psup,
                                                 aTri.data(), aTri.size()/3, 3,
-                                                (int)aXYZ.size()/3);
+                                                (int)aXYZ0.size()/3);
     dfm2::JArray_Sort(psup_ind, psup);
-    mat_A.Initialize(aXYZ.size()/3, 3, true);
+    mat_A.Initialize(aXYZ0.size()/3, 3, true);
     mat_A.SetPattern(psup_ind.data(), psup_ind.size(),
                      psup.data(),     psup.size());
   }
-  aDelta.resize(aXYZ.size());
+  aDelta.resize(aXYZ0.size());
   SetLinSys_LaplaceGraph_MeshTri3D(mat_A, aDelta,
-                                   aXYZ.data(), aXYZ.size() / 3,
+                                   aXYZ0.data(), aXYZ0.size() / 3,
                                    aTri.data(), aTri.size() / 3);
   {
-    const unsigned int np = aXYZ.size() / 3;
+    const unsigned int np = aXYZ0.size() / 3;
     aBCFlag.assign(np * 3, 0);
-    aBCFlag[0] = 1;
-    aBCFlag[1] = 1;
-    aBCFlag[2] = 1;
-    aBCFlag[(np - 1) * 3 + 0] = 1;
-    aBCFlag[(np - 1) * 3 + 1] = 1;
-    aBCFlag[(np - 1) * 3 + 2] = 1;
+    for(unsigned int ip=0;ip<np;++ip) {
+      double x0 = aXYZ0[ip*3+0];
+      if( x0 < -0.4 ){
+        aBCFlag[ip*3+0] = 1;
+        aBCFlag[ip*3+1] = 1;
+        aBCFlag[ip*3+2] = 1;
+      }
+      if( x0 > +0.4 ){
+        aBCFlag[ip*3+0] = 2;
+        aBCFlag[ip*3+1] = 2;
+        aBCFlag[ip*3+2] = 2;
+      }
+    }
   }
 }
 
 // -------------------------------------------
-
 // -------------------------------------------
-
 
 void myGlutDisplay()
 {
   ::glDisable(GL_LIGHTING);
   ::glColor3d(1,0,0);
-  dfm2::opengl::DrawMeshTri3D_FaceNorm(aXYZ,aTri);
+  dfm2::opengl::DrawMeshTri3D_FaceNorm(aXYZ0,aTri);
   ::glColor3d(0,0,0);
-  dfm2::opengl::DrawMeshTri3D_Edge(aXYZ.data(),aXYZ.size()/3, aTri.data(),aTri.size()/3);
+  dfm2::opengl::DrawMeshTri3D_Edge(aXYZ0.data(),aXYZ0.size()/3, aTri.data(),aTri.size()/3);
   dfm2::opengl::DrawMeshTri3D_Edge(aXYZ1.data(),aXYZ1.size()/3,aTri.data(),aTri.size()/3);
 }
 
@@ -180,12 +183,22 @@ int main(int argc,char* argv[])
     {
       std::vector<double> aRhs = aDelta;
       const unsigned int np = aRhs.size()/3;
-      aRhs[0*3+1] = -0.75;
-      aRhs[(np-1)*3+1] = +1.0+cos(iframe*0.01);
-      aRhs[(np-1)*3+0] = +sin(iframe*0.01);
+      for(unsigned int ip=0;ip<np;++ip){
+        if( aBCFlag[ip*3+0] == 0 ){ continue; }
+        if( aBCFlag[ip*3+0] == 1 ){
+          aRhs[ip*3+0] = aXYZ0[ip*3+0];
+          aRhs[ip*3+1] = aXYZ0[ip*3+1];
+          aRhs[ip*3+2] = aXYZ0[ip*3+2];
+        }
+        if( aBCFlag[ip*3+0] == 2 ) {
+          aRhs[ip*3+0] = aXYZ0[ip*3+0]+0.1*sin(0.1*iframe);
+          aRhs[ip*3+1] = aXYZ0[ip*3+1]+0.3*cos(0.1*iframe);
+          aRhs[ip*3+2] = aXYZ0[ip*3+2];
+        }
+      }
       mat_A.SetFixedBC_Dia(aBCFlag.data());
       mat_A.SetFixedBC_Row(aBCFlag.data());
-      aXYZ1 = aXYZ;
+      aXYZ1 = aXYZ0;
       std::vector<double> aRes = Solve_BiCGSTAB(aRhs,
                                                 aXYZ1, 1.0e-5, 100, mat_A);
       std::cout << aRes.size() << std::endl;
