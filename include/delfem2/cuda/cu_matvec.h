1cd358a6462b25fba7005213f6b3df80230eda45
Nobuyuki Umetani <n.umetani@gmail.com>
2020-01-14 00:42:47 +0900

    namespace for cuda

--- include/delfem2/cuda/cu_matvec.h
+++ include/delfem2/cuda/cu_matvec.h
@@ -1,11 +1,12 @@
-#include <cstdio>
-#include <cstdlib>
 #include "cuda_runtime.h"
 
-__global__
-void kernel_VecScale(float *out, float *in, float scale, const int n);
+namespace delfem2 {
+namespace cuda{
 
-void cuda_VecScale(float *hOut, float *hIn, float scale, const int n);
+__global__
+void kernel_VecScale(float *out, const float *in, float scale, const int n);
+
+void cuda_VecScale(float *hOut, const float *hIn, float scale, const int n);
 
 // -------------------------------------------------------------
 
@@ -13,15 +14,15 @@ void cuda_VecScale(float *hOut, float *hIn, float scale, const int n);
  * @brief dot product of two vectors
  */
 __global__
-void cuda_Dot_TPB64(
-    float* d_res,
-    const float* d_A,
-    const float* d_B,
+void kernel_Dot_TPB64(
+    float *d_res,
+    const float *d_A,
+    const float *d_B,
     int n);
 
 float cuda_Dot(
-    const float* h_A,
-    const float* h_B,
+    const float *h_A,
+    const float *h_B,
     unsigned int n);
 
 // -------------------------------------------------------------
@@ -34,7 +35,10 @@ void kernel_MatMat_TPB16(
     unsigned int WIDTH);
 
 void cuda_MatMat(
-    float* h_C_gpu,
-    const float* h_A,
-    const float* h_B,
+    float *h_C_gpu,
+    const float *h_A,
+    const float *h_B,
     unsigned int WIDTH);
+
+} // cuda
+} // delfem2
