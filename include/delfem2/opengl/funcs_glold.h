b36da080dbf678dcfa080e4058ef01423211fdc9
Mad Daddy <maddady@gmail.com>
2020-06-14 22:29:02 +0300

    *fix mod build

--- include/delfem2/opengl/funcs_glold.h
+++ include/delfem2/opengl/funcs_glold.h
@@ -17,6 +17,13 @@
 #include <string>
 #include <vector>
 
+ /// <summary>
+ /// test fix
+#ifndef M_PI
+#define M_PI 3.14159265358979323846264338327950288
+#endif
+/// </summary>
+
 namespace delfem2{
 namespace opengl{
 
--- include/delfem2/primitive.h
+++ include/delfem2/primitive.h
@@ -13,6 +13,13 @@
 #include "delfem2/sdf.h"
 #include "delfem2/dfm2_inline.h"
 
+/// <summary>
+/// test fix
+#ifndef M_PI
+#  define M_PI 3.14159265358979323846264338327950288
+#endif
+/// </summary>
+
 namespace delfem2 {
 
 // -----------------------
