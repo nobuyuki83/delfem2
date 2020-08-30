235af126d79fb80282905c8ca93684ee09359fc1
Nobuyuki Umetani <n.umetani@gmail.com>
2020-08-30 22:25:40 +0900

    fixed fdmfluid2d

--- examples_glfwold/608_FdmFluid2D/main.cpp
+++ examples_glfwold/608_FdmFluid2D/main.cpp
@@ -8,27 +8,28 @@
 
 #include <iostream>
 #include <vector>
-#include <math.h>
+#include <cmath>
+#include <random>
 // ------
 #include <GLFW/glfw3.h>
 #include "delfem2/opengl/glfw/viewer_glfw.h"
 
-unsigned int max(unsigned int i, unsigned int j) {
-  return (i>j?i:j);
-}
+template <typename VAL>
+VAL max(VAL i, VAL j) { return (i>j?i:j); }
 
-unsigned int min(unsigned int i,unsigned int j){
-  return (i>j?j:i);
-}
+template <typename VAL>
+VAL min(VAL i, VAL j) { return (i>j?j:i); }
 
 // Clamped Fetch
 double ClampedFetch(
     double* x,
     unsigned int i, 
-    unsigned int j, unsigned int nw, unsigned int nh)
+    unsigned int j,
+    unsigned int nw,
+    unsigned int nh)
 {
-	i = min(max(0,i),nw-1);
-	j = min(max(0,j),nh-1);
+	i = min(max((unsigned int)0,i),nw-1);
+	j = min(max((unsigned int)0,j),nh-1);
 	return x[i+j*nw];
 }
 
@@ -36,19 +37,23 @@ double ClampedFetch(
 void GaussSeidelSolver(
     double* p,
     const double* d,
-    int n, unsigned int K0 )
+    unsigned int n,
+    unsigned int K0 )
 {
 	double h2 = 1.0/(n*n);
 	for(unsigned int k=0; k<K0; k++ ){
     double t = 0;
-		for(int i=0; i<n; i++ ){
-    for(int j=0; j<n; j++ ){
-      const double p0 = p[i+j*n];
-			p[i+j*n] = (ClampedFetch(p,i+1,j,n,n)+ClampedFetch(p,i-1,j,n,n)+ClampedFetch(p,i,j+1,n,n)+ClampedFetch(p,i,j-1,n,n)
-                  -h2*d[i+j*n]) / 4.0;
-      const double p1 = p[i+j*n];
-      t += fabs(p0-p1);
-		}
+		for(unsigned int i=0; i<n; i++ ){
+      for(unsigned int j=0; j<n; j++ ){
+        const double p0 = p[i+j*n];
+        p[i+j*n] = (
+            ClampedFetch(p,i+1,j,n,n) +
+            ClampedFetch(p,i-1,j,n,n) +
+            ClampedFetch(p,i,j+1,n,n) +
+            ClampedFetch(p,i,j-1,n,n) - h2*d[i+j*n] ) / 4.0;
+        const double p1 = p[i+j*n];
+        t += fabs(p0-p1);
+      }
     }
     if( k % 100 == 0 ){
       std::cout << "itr : " << k << " " << t << std::endl;
@@ -56,52 +61,26 @@ void GaussSeidelSolver(
 	}
 }
 
-
-const unsigned int ngrid = 32;
-std::vector<double> velou;  // (ngrid+1)*ngrid
-std::vector<double> velov;  // ngrid*(ngrid+1)
-std::vector<double> press;  // ngrid*ngrid
-std::vector<double> divag;  // ngrid*ngrid;
-const double dt = 0.1;
-const double rho = 1;
-bool is_animation = true;
-const double gravity[2] = {0,-0.01};
-
-std::vector<double> velou_tmp;  // (ngrid+1)*ngrid*2
-std::vector<double> velov_tmp;  // ngrid*(ngrid+1)*2
-
-std::vector<double> aMarker;
-
-void ClearValue(){
-  for(unsigned int i=0;i<ngrid*(ngrid+1);i++){ velou[i] = 0; }
-  for(unsigned int i=0;i<ngrid*(ngrid+1);i++){ velov[i] = 0; }  
-  for(unsigned int i=0;i<ngrid*ngrid;i++){ press[i] = 0; }
-  for(unsigned int i=0;i<ngrid*ngrid;i++){ divag[i] = 0; }  
-  aMarker.clear();
-}
-
-void InitSimulation()
+void CompDivergence(
+    std::vector<double>& divag,
+    unsigned int ngrid,
+    const std::vector<double>& velou,
+    const std::vector<double>& velov)
 {
-  velou.resize(ngrid*(ngrid+1));
-  velov.resize(ngrid*(ngrid+1));
-  press.resize(ngrid*ngrid);
-  divag.resize(ngrid*ngrid);
-  velou_tmp.resize(ngrid*(ngrid+1)*2);
-  velov_tmp.resize(ngrid*(ngrid+1)*2);
-  ClearValue();
-}
-
-void CompDivergence() {
   for(unsigned int ig=0;ig<ngrid;ig++){
-  for(unsigned int jg=0;jg<ngrid;jg++){    
-		double div = (velou[ig+1+jg*(ngrid+1)]-velou[ig+jg*(ngrid+1)]) 
-               + (velov[ig+(jg+1)*ngrid]-velov[ig+jg*ngrid]);
-		divag[ig+jg*ngrid] = div*ngrid;
-  }
+    for(unsigned int jg=0;jg<ngrid;jg++){
+      double div = (velou[ig+1+jg*(ngrid+1)]-velou[ig+jg*(ngrid+1)])
+                 + (velov[ig+(jg+1)*ngrid]-velov[ig+jg*ngrid]);
+      divag[ig+jg*ngrid] = div*ngrid;
+    }
 	}
 }
 
-void EnforceBoundary(){
+void EnforceBoundary(
+    std::vector<double>& velou,
+    std::vector<double>& velov,
+    unsigned int ngrid)
+{
   for(unsigned int ig=0;ig<ngrid;ig++){ velov[ig] = 0; }
   for(unsigned int ig=0;ig<ngrid;ig++){ velov[ig+ngrid*ngrid] = 0; }  
   for(unsigned int jg=0;jg<ngrid;jg++){ 
@@ -110,101 +89,116 @@ void EnforceBoundary(){
   }
 }
 
-void CompPressure(){
+void CompPressure(
+    std::vector<double>& divag,
+    std::vector<double>& press,
+    unsigned int ngrid,
+    double rho,
+    double dt)
+{
   const double dtmp1 = rho/dt;
   for(unsigned int ig=0;ig<ngrid;ig++){
-  for(unsigned int jg=0;jg<ngrid;jg++){     
-		divag[ig+jg*ngrid] *= dtmp1;    
-  }
+    for(unsigned int jg=0;jg<ngrid;jg++){
+      divag[ig+jg*ngrid] *= dtmp1;
+    }
   }
   GaussSeidelSolver(press.data(), divag.data(), ngrid, 1000);
 }
 
-void SubtractPressure(){
+void SubtractPressure(
+    std::vector<double>& velou,
+    std::vector<double>& velov,
+    unsigned int ngrid,
+    double dt,
+    double rho,
+    const std::vector<double>& press)
+{
   double dtmp1 = dt/rho;
   for(unsigned int ig=0;ig<ngrid-1;ig++){
-  for(unsigned int jg=0;jg<ngrid;jg++){    
-    velou[(ig+1)+jg*(ngrid+1)] -= dtmp1*(press[(ig+1)+jg*ngrid]-press[ig+jg*ngrid])*ngrid;
-  }
+    for(unsigned int jg=0;jg<ngrid;jg++){
+      velou[(ig+1)+jg*(ngrid+1)] -= dtmp1*(press[(ig+1)+jg*ngrid]-press[ig+jg*ngrid])*ngrid;
+    }
   }
   for(unsigned int ig=0;ig<ngrid;ig++){
-  for(unsigned int jg=0;jg<ngrid-1;jg++){
-    velov[ig+(jg+1)*ngrid] -= dtmp1*(press[ig+(jg+1)*ngrid]-press[ig+(jg+0)*ngrid])*ngrid;
-  }
+    for(unsigned int jg=0;jg<ngrid-1;jg++){
+      velov[ig+(jg+1)*ngrid] -= dtmp1*(press[ig+(jg+1)*ngrid]-press[ig+(jg+0)*ngrid])*ngrid;
+    }
   }  
 }
 
 
-static double linear_interpolate (double* d, unsigned int ndim, unsigned int idim, int nw, int nh, double x, double y ) 
+double linear_interpolate (
+    const double* d,
+    unsigned int ndim,
+    unsigned int idim,
+    unsigned int nw,
+    unsigned int nh,
+    double x,
+    double y )
 {
-	x = max(0.0,min(nw,x));
-	y = max(0.0,min(nh,y));
-	int i = min(x,nw-2);
-	int j = min(y,nh-2);
+	x = max(0.0,min((double)nw,x));
+	y = max(0.0,min((double)nh,y));
+	int i = min(x,(double)nw-2);
+	int j = min(y,(double)nh-2);
 	return ( (i+1-x)*d[(i+j*nw    )*ndim+idim]+(x-i)*d[(i+1+j*nw    )*ndim+idim])*(j+1-y) 
         + ((i+1-x)*d[(i+(j+1)*nw)*ndim+idim]+(x-i)*d[(i+1+(j+1)*nw)*ndim+idim])*(y-j);
 }
 
-void CompAdvectionSemiLagrangian()
+void CompAdvectionSemiLagrangian(
+    std::vector<double>& velou,
+    std::vector<double>& velov,
+    std::vector<double>& velou_tmp,
+    std::vector<double>& velov_tmp,
+    unsigned int ngrid,
+    double dt)
 {  
   for(unsigned int jg=0;jg<ngrid+0;jg++){
-  for(unsigned int ig=0;ig<ngrid+1;ig++){
-		velou_tmp[(ig+jg*(ngrid+1))*2+0] = velou[ig+jg*(ngrid+1)];
-		velou_tmp[(ig+jg*(ngrid+1))*2+1] = ( ClampedFetch(velov.data(), ig-1,jg,   ngrid,ngrid+1)
-                                        +ClampedFetch(velov.data(), ig,  jg,   ngrid,ngrid+1)
-                                        +ClampedFetch(velov.data(), ig-1,jg+1, ngrid,ngrid+1)
-                                        +ClampedFetch(velov.data(), ig,  jg+1, ngrid,ngrid+1) )*0.25;
-  }
+    for(unsigned int ig=0;ig<ngrid+1;ig++){
+      velou_tmp[(ig+jg*(ngrid+1))*2+0] = velou[ig+jg*(ngrid+1)];
+      velou_tmp[(ig+jg*(ngrid+1))*2+1] = ( ClampedFetch(velov.data(), ig-1,jg,   ngrid,ngrid+1)
+                                          +ClampedFetch(velov.data(), ig,  jg,   ngrid,ngrid+1)
+                                          +ClampedFetch(velov.data(), ig-1,jg+1, ngrid,ngrid+1)
+                                          +ClampedFetch(velov.data(), ig,  jg+1, ngrid,ngrid+1) )*0.25;
+    }
 	}
   
   for(unsigned int jg=0;jg<ngrid+1;jg++){
-  for(unsigned int ig=0;ig<ngrid+0;ig++){
-    velov_tmp[(ig+jg*ngrid)*2+0] = ( ClampedFetch(velou.data(), ig,  jg-1,ngrid+1,ngrid)
-                                    +ClampedFetch(velou.data(), ig+1,jg-1,ngrid+1,ngrid)
-                                    +ClampedFetch(velou.data(), ig,  jg,  ngrid+1,ngrid)
-                                    +ClampedFetch(velou.data(), ig+1,jg,  ngrid+1,ngrid) )*0.25;
-    velov_tmp[(ig+jg*ngrid)*2+1] = velov[ig+jg*ngrid];    
-  }
-	}
-  
-  {
-    const unsigned int nMark = aMarker.size()/2;
-    for(unsigned int imark=0;imark<nMark;imark++){
-      const double q0[2] = {aMarker[imark*2+0]*ngrid,aMarker[imark*2+1]*ngrid};
-      const double u = linear_interpolate(velou_tmp.data(),2,0,ngrid+1,ngrid,q0[0],q0[1]);
-      const double v = linear_interpolate(velov_tmp.data(),2,1,ngrid,ngrid+1,q0[0],q0[1]);
-      const double p0[2] = {aMarker[imark*2+0],aMarker[imark*2+1]};
-      double p1[2] = {p0[0]+u*dt,p0[1]+v*dt};
-      if( p1[0] < 0 ){ p1[0] = -p1[0]; }
-      if( p1[0] > 1 ){ p1[0] = 2-p1[0]; }      
-      if( p1[1] < 0 ){ p1[1] = -p1[1]; }
-      if( p1[1] > 1 ){ p1[1] = 2-p1[1]; }            
-      aMarker[imark*2+0] = p1[0];
-      aMarker[imark*2+1] = p1[1];
+    for(unsigned int ig=0;ig<ngrid+0;ig++){
+      velov_tmp[(ig+jg*ngrid)*2+0] = ( ClampedFetch(velou.data(), ig,  jg-1,ngrid+1,ngrid)
+                                      +ClampedFetch(velou.data(), ig+1,jg-1,ngrid+1,ngrid)
+                                      +ClampedFetch(velou.data(), ig,  jg,  ngrid+1,ngrid)
+                                      +ClampedFetch(velou.data(), ig+1,jg,  ngrid+1,ngrid) )*0.25;
+      velov_tmp[(ig+jg*ngrid)*2+1] = velov[ig+jg*ngrid];
     }
-  }
-  
+	}
+
   for(unsigned int jg=0;jg<ngrid+0;jg++){
-  for(unsigned int ig=0;ig<ngrid+1;ig++){
-    const double* velo = velou_tmp.data()+(ig+jg*(ngrid+1))*2;
-    const double p[2] = {ig-velo[0]*dt*ngrid,jg-velo[1]*dt*ngrid};    
-    const double u = linear_interpolate(velou_tmp.data(),2,0,ngrid+1,ngrid,p[0],p[1]);
-    velou[ig+jg*(ngrid+1)] = u;
-  }
+    for(unsigned int ig=0;ig<ngrid+1;ig++){
+      const double* velo = velou_tmp.data()+(ig+jg*(ngrid+1))*2;
+      const double p[2] = {ig-velo[0]*dt*ngrid,jg-velo[1]*dt*ngrid};
+      const double u = linear_interpolate(velou_tmp.data(),2,0,ngrid+1,ngrid,p[0],p[1]);
+      velou[ig+jg*(ngrid+1)] = u;
+    }
   }
 
   for(unsigned int jg=0;jg<ngrid+1;jg++){
-  for(unsigned int ig=0;ig<ngrid+0;ig++){
-    const double* velo = velov_tmp.data()+(ig+jg*ngrid)*2;
-    const double p[2] = {ig-velo[0]*dt*ngrid,jg-velo[1]*dt*ngrid};    
-    const double v = linear_interpolate(velov_tmp.data(),2,1,ngrid,ngrid+1,p[0],p[1]);
-    velov[ig+jg*ngrid] = v;
-  }
+    for(unsigned int ig=0;ig<ngrid+0;ig++){
+      const double* velo = velov_tmp.data()+(ig+jg*ngrid)*2;
+      const double p[2] = {ig-velo[0]*dt*ngrid,jg-velo[1]*dt*ngrid};
+      const double v = linear_interpolate(velov_tmp.data(),2,1,ngrid,ngrid+1,p[0],p[1]);
+      velov[ig+jg*ngrid] = v;
+    }
   }
 }
 
 
-void AssignGravity(){
+void AssignGravity(
+    std::vector<double>& velou,
+    std::vector<double>& velov,
+    unsigned int ngrid,
+    const double gravity[2],
+    double dt)
+{
   for(unsigned int jg=0;jg<ngrid+0;jg++){
     for(unsigned int ig=0;ig<ngrid+1;ig++){
       velou[ig+jg*(ngrid+1)] += gravity[0]*dt;
@@ -219,22 +213,13 @@ void AssignGravity(){
   
 }
 
-  
-void StepTime(){
-  AssignGravity();
-  EnforceBoundary();
-  CompDivergence();
-  CompPressure();
-  SubtractPressure();
-  CompAdvectionSemiLagrangian();
-}
-
-void glutMyDisplay(void){
-  if( is_animation ){
-    StepTime();
-  }
-	glClear(GL_COLOR_BUFFER_BIT);  
-
+void glutMyDisplay(
+    unsigned int ngrid,
+    const std::vector<double>& velou,
+    const std::vector<double>& velov,
+    const std::vector<double>& press)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
   {
     double h = 1.0/ngrid;    
     ::glBegin(GL_QUADS);
@@ -257,152 +242,40 @@ void glutMyDisplay(void){
     ::glColor3d(0,1,1);
     ::glBegin(GL_LINES);
     for(unsigned int ig=0;ig<ngrid;ig++){
-    for(unsigned int jg=0;jg<ngrid;jg++){
-      const double p[2] = {(ig+0.5)*h,(jg+0.5)*h};
-      const double u = (velou[ig+jg*(ngrid+1)] + velou[ig+1+ jg   *(ngrid+1)])*0.5*3;
-      const double v = (velov[ig+jg*ngrid    ] + velov[ig+  (jg+1)* ngrid   ])*0.5*3;
-      ::glVertex2d(p[0],  p[1]  );
-      ::glVertex2d(p[0]+u,p[1]+v);
-    }
-    }
-    ::glEnd();
-  }  
-  /*
-  {
-    double h = 1.0/ngrid;
-    ::glBegin(GL_POINTS);
-    for(unsigned int ig=0;ig<ngrid;ig++){
-    for(unsigned int jg=0;jg<ngrid;jg++){
-      const double p[2] = {(ig+0.5)*h,(jg+0.5)*h};    
-      ::glVertex2d(p[0],p[1]);
-    }    
+      for(unsigned int jg=0;jg<ngrid;jg++){
+        const double p[2] = {(ig+0.5)*h,(jg+0.5)*h};
+        const double u = (velou[ig+jg*(ngrid+1)] + velou[ig+1+ jg   *(ngrid+1)])*0.5*3;
+        const double v = (velov[ig+jg*ngrid    ] + velov[ig+  (jg+1)* ngrid   ])*0.5*3;
+        ::glVertex2d(p[0],  p[1]  );
+        ::glVertex2d(p[0]+u,p[1]+v);
+      }
     }
     ::glEnd();
   }
-   */
-  {
-    const unsigned int nMark = aMarker.size()/2;
-    ::glColor3d(1,0,0);
-    ::glPointSize(5);
-    ::glBegin(GL_POINTS);
-    for(unsigned int imark=0;imark<nMark;imark++){
-      const double p[2] = {aMarker[imark*2+0],aMarker[imark*2+1]};           
-      ::glVertex3d(p[0],p[1],0.1);
-    }    
-    ::glEnd();
-  }
 }
-
-/*
-static void init( int gsize ) {
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-}
- */
-
-/*
-static void glutMyReshape(int w, int h) {
-  ::glViewport(0,0,w,h);
-  ::glMatrixMode(GL_PROJECTION);
-  ::glLoadIdentity();
-  ::glOrtho(0,1, 0,1, -1,1);
-}
-
-static void glutMyIdle( void ) {
-
-	glutPostRedisplay();
-}
-
-static void glutMyKeyboard( unsigned char key, int x, int y ) {
-  if( key == ' ' ){
-    ClearValue();
-  }
-  if( key == 'b' ){
-    const double h = 1.0/ngrid;
-    for(unsigned int jg=0;jg<ngrid;jg++){
-    for(unsigned int ig=0;ig<ngrid+1;ig++){    
-      const double p[2] = {ig*h,(jg+0.5)*h};
-      const double r = sqrt( (p[0]-0.5)*(p[0]-0.5) + (p[1]-0.5)*(p[1]-0.5) );
-      velou[ig+jg*(ngrid+1)] += max(0,0.001-r*r);
-    }
-    }
-  }
-  if( key == 'c' ){
-    StepTime();
-  }
-  else if( key == 'a' ){
-    is_animation = !is_animation;
-  }
-}
-
-int prev_x, prev_y;
-int imodifier;
-
-static void glutMyMouse ( int button, int state, int x, int y ) {
-	prev_x = x;
-	prev_y = y;
-  imodifier = glutGetModifiers();
-  if( imodifier == GLUT_ACTIVE_SHIFT ){
-    int w[4];
-    ::glGetIntegerv(GL_VIEWPORT,w);
-    int win_x = w[2];
-    int win_y = w[3];        
-    const double px = x/(GLdouble)win_x;
-    const double py = 1.0 - y/(GLdouble)win_y;
-    aMarker.push_back(px);
-    aMarker.push_back(py);
-  }
-}
- */
-
-void SetVeloFieldAround(double x, double y, double u, double v){
-  const double h = 1.0/ngrid;
-  for(unsigned int jg=0;jg<ngrid;jg++){
-  for(unsigned int ig=0;ig<ngrid+1;ig++){    
-    const double p[2] = {ig*h,(jg+0.5)*h};
-    const double r = sqrt( (p[0]-x)*(p[0]-x) + (p[1]-y)*(p[1]-y) );
-    velou[ig+jg*(ngrid+1)] += max(0,0.01-r*r)*u*100;
-  }
-  }  
-  for(unsigned int jg=0;jg<ngrid+1;jg++){
-  for(unsigned int ig=0;ig<ngrid;ig++){    
-    const double p[2] = {(ig+0.5)*h,jg*h};
-    const double r = sqrt( (p[0]-x)*(p[0]-x) + (p[1]-y)*(p[1]-y) );
-    velov[ig+jg*ngrid] += max(0,0.01-r*r)*v*100;
-  }
-  }    
-}
-
-/*
-static void glutMyMotion ( int x, int y ) {
-  if( imodifier == GLUT_ACTIVE_SHIFT ){
-    int w[4];
-    ::glGetIntegerv(GL_VIEWPORT,w);
-    int win_x = w[2];
-    int win_y = w[3];        
-    const double px = x/(GLdouble)win_x;
-    const double py = 1.0 - y/(GLdouble)win_y;
-    aMarker.push_back(px);
-    aMarker.push_back(py);
-  }
-  else{
-    int w[4];
-    ::glGetIntegerv(GL_VIEWPORT,w);
-    int win_x = w[2];
-    int win_y = w[3];    
-    SetVeloFieldAround(x/(GLdouble)win_x, 1.0 - y/(GLdouble)win_y,
-                       (x-prev_x)/(GLdouble)win_x, -(y-prev_y)/(GLdouble)win_y );
-	}
-	prev_x = x;
-	prev_y = y;
-}
- */
 
 
 int main (int argc, char * argv[])
 {
-  InitSimulation();
-  ClearValue();
-  
+  const unsigned int ngrid = 32;
+  std::vector<double> velou(ngrid*(ngrid+1), 0.0);  // (ngrid+1)*ngrid
+  std::vector<double> velov(ngrid*(ngrid+1), 0.0);  // ngrid*(ngrid+1)
+  std::vector<double> press(ngrid*ngrid, 0.0);  // ngrid*ngrid
+  std::vector<double> divag(ngrid*ngrid, 0.0);  // ngrid*ngrid;
+  std::vector<double> velou_tmp;  // (ngrid+1)*ngrid*2
+  std::vector<double> velov_tmp;  // ngrid*(ngrid+1)*2
+  // -----------
+  // allocate memory
+  velou_tmp.resize(ngrid*(ngrid+1)*2);
+  velov_tmp.resize(ngrid*(ngrid+1)*2);
+  // -----------
+  const double dt = 0.1;
+  const double rho = 1.0;
+  const double gravity[2] = {0,-0.01};
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  std::uniform_real_distribution<> dist(-1.0, 1.0);
+
   delfem2::opengl::CViewer_GLFW viewer;
   viewer.Init_oldGL();
   
@@ -411,11 +284,22 @@ int main (int argc, char * argv[])
   {
     iframe = (iframe+1)%100;
     if( iframe == 0 ){
-      for(unsigned int i=0;i<ngrid*(ngrid+1);i++){ velou[i] = 2.0*rand()/(RAND_MAX+1.0)-1.0; }
-      for(unsigned int i=0;i<ngrid*(ngrid+1);i++){ velov[i] = 2.0*rand()/(RAND_MAX+1.0)-1.0; }
+      for(unsigned int i=0;i<ngrid*(ngrid+1);i++){ velou[i] = dist(mt); }
+      for(unsigned int i=0;i<ngrid*(ngrid+1);i++){ velov[i] = dist(mt); }
     }
+    // ----
+    AssignGravity(velou,velov,ngrid,gravity,dt);
+    EnforceBoundary(velou,velov, ngrid);
+    CompDivergence(divag, ngrid, velou, velov);
+    CompPressure(divag,press,
+        ngrid,rho,dt);
+    SubtractPressure(velou,velov,
+        ngrid,dt,rho,press);
+    CompAdvectionSemiLagrangian(velou,velov,velou_tmp,velov_tmp,
+        ngrid,dt);
+    // ----
     viewer.DrawBegin_oldGL();
-    glutMyDisplay();
+    glutMyDisplay(ngrid,velou,velov,press);
     viewer.DrawEnd_oldGL();
     if( glfwWindowShouldClose(viewer.window) ) goto EXIT;
   }
