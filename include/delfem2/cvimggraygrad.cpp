4e7056ba683af0523732335652a6ca187526ed6d
Nobuyuki Umetani <n.umetani@gmail.com>
2021-02-07 22:11:45 +0900

    small changes

--- include/delfem2/cvimggraygrad.cpp
+++ include/delfem2/cvimggraygrad.cpp
@@ -3,9 +3,16 @@
 #include "delfem2/viewtensor.h"
 #include "delfem2/dfm2_inline.h"
 #include <cassert>
+#include <cmath>
 
-inline float Dot3(const float p[3], const float q[3]){
+namespace delfem2 {
+namespace cvimggraygrad {
+
+inline float Dot3(const float p[3], const float q[3]) {
   return p[0] * q[0] + p[1] * q[1] + p[2] * q[2];
+}
+
+}
 }
 
 DFM2_INLINE void delfem2::Grayscale(
@@ -22,7 +29,7 @@ DFM2_INLINE void delfem2::Grayscale(
   for (int iv = 0; iv < nv; ++iv) {
     for (int iy1 = 0; iy1 < ny; ++iy1) {
       for (int ix1 = 0; ix1 < nx; ++ix1) {
-        aGray(iv,iy1,ix1) = Dot3(frame.data(iv, iy1, ix1), gray_coeff);
+        aGray(iv,iy1,ix1) = cvimggraygrad::Dot3(frame.data(iv, iy1, ix1), gray_coeff);
       }
     }
   }
@@ -51,10 +58,10 @@ DFM2_INLINE void delfem2::GrayscaleGradientCenter(
         assert( iy0 >=0 && iy0 < ny );
         assert( ix1 >=0 && ix1 < nx );
         assert( iy1 >=0 && iy1 < ny );
-        const auto v01 = Dot3(frame.data(iv,iy0,ix1),gray_coeff);
-        const auto v10 = Dot3(frame.data(iv,iy1,ix0),gray_coeff);
-        const auto v12 = Dot3(frame.data(iv,iy1,ix2),gray_coeff);
-        const auto v21 = Dot3(frame.data(iv,iy2,ix1),gray_coeff);
+        const auto v01 = cvimggraygrad::Dot3(frame.data(iv,iy0,ix1),gray_coeff);
+        const auto v10 = cvimggraygrad::Dot3(frame.data(iv,iy1,ix0),gray_coeff);
+        const auto v12 = cvimggraygrad::Dot3(frame.data(iv,iy1,ix2),gray_coeff);
+        const auto v21 = cvimggraygrad::Dot3(frame.data(iv,iy2,ix1),gray_coeff);
         aGrad(iv, iy1, ix1, 0) = (v12 - v10)*0.5;
         aGrad(iv, iy1, ix1, 1) = (v21 - v01)*0.5;
       }
@@ -84,14 +91,14 @@ DFM2_INLINE void delfem2::GrayscaleGradientSobel(
         assert( iy0 >=0 && iy0 < ny );
         assert( ix1 >=0 && ix1 < nx );
         assert( iy1 >=0 && iy1 < ny );
-        const auto v00 = Dot3(frame.data(iv,iy0,ix0),gray_coeff);
-        const auto v01 = Dot3(frame.data(iv,iy0,ix1),gray_coeff);
-        const auto v02 = Dot3(frame.data(iv,iy0,ix2),gray_coeff);
-        const auto v10 = Dot3(frame.data(iv,iy1,ix0),gray_coeff);
-        const auto v12 = Dot3(frame.data(iv,iy1,ix2),gray_coeff);
-        const auto v20 = Dot3(frame.data(iv,iy2,ix0),gray_coeff);
-        const auto v21 = Dot3(frame.data(iv,iy2,ix1),gray_coeff);
-        const auto v22 = Dot3(frame.data(iv,iy2,ix2),gray_coeff);
+        const auto v00 = cvimggraygrad::Dot3(frame.data(iv,iy0,ix0),gray_coeff);
+        const auto v01 = cvimggraygrad::Dot3(frame.data(iv,iy0,ix1),gray_coeff);
+        const auto v02 = cvimggraygrad::Dot3(frame.data(iv,iy0,ix2),gray_coeff);
+        const auto v10 = cvimggraygrad::Dot3(frame.data(iv,iy1,ix0),gray_coeff);
+        const auto v12 = cvimggraygrad::Dot3(frame.data(iv,iy1,ix2),gray_coeff);
+        const auto v20 = cvimggraygrad::Dot3(frame.data(iv,iy2,ix0),gray_coeff);
+        const auto v21 = cvimggraygrad::Dot3(frame.data(iv,iy2,ix1),gray_coeff);
+        const auto v22 = cvimggraygrad::Dot3(frame.data(iv,iy2,ix2),gray_coeff);
         const float dx = (-v00 - 2*v10 - v20 + v02 + 2*v12 + v22)/8.f;
         const float dy = (-v00 - 2*v01 - v02 + v20 + 2*v21 + v22)/8.f;
         aGrad(iv, iy1, ix1, 0) = dx;
