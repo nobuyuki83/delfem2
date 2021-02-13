906b0d821a24854770b9f4a29028bec56e93a1a5
Nobuyuki Umetani <n.umetani@gmail.com>
2021-02-14 07:52:20 +0900

    small

--- include/delfem2/opengl/old/tex.h
+++ include/delfem2/opengl/old/tex.h
@@ -125,10 +125,23 @@ void DrawTex_CvNormDist(
   ::glBegin(GL_QUADS);
   for (unsigned int iw = 0; iw < nw - 1; ++iw) {
     for (unsigned int ih = 0; ih < nh - 1; ++ih) {
-      double z0 = -aNormDist[((ih + 0) * nw + (iw + 0))*4+3] * ratio0;
-      double z1 = -aNormDist[((ih + 0) * nw + (iw + 1))*4+3] * ratio0;
-      double z2 = -aNormDist[((ih + 1) * nw + (iw + 1))*4+3] * ratio0;
-      double z3 = -aNormDist[((ih + 1) * nw + (iw + 0))*4+3] * ratio0;
+      const float x0[3] = {float(iw)+0.5f,float(ih)+0.5f,1.f};
+      const float x1[3] = {float(iw)+1.5f,float(ih)+0.5f,1.f};
+      const float x2[3] = {float(iw)+1.5f,float(ih)+1.5f,1.f};
+      const float x3[3] = {float(iw)+0.5f,float(ih)+1.5f,1.f};
+      float Kix0[3]; MatVec3(Kix0, Kinv, x0);
+      float Kix1[3]; MatVec3(Kix1, Kinv, x1);
+      float Kix2[3]; MatVec3(Kix2, Kinv, x2);
+      float Kix3[3]; MatVec3(Kix3, Kinv, x3);
+      float ntKix0 = Dot3(aNormDist.data()+((ih + 0) * nw + (iw + 0))*4, Kix0);
+      float ntKix1 = Dot3(aNormDist.data()+((ih + 0) * nw + (iw + 1))*4, Kix1);
+      float ntKix2 = Dot3(aNormDist.data()+((ih + 1) * nw + (iw + 1))*4, Kix2);
+      float ntKix3 = Dot3(aNormDist.data()+((ih + 1) * nw + (iw + 0))*4, Kix3);
+//      std::cout << ntKix0 << " " << ntKix1 << " " << ntKix2 << " " << ntKix3 << std::endl;
+      double z0 = -aNormDist[((ih + 0) * nw + (iw + 0))*4+3] / ntKix0;
+      double z1 = -aNormDist[((ih + 0) * nw + (iw + 1))*4+3] / ntKix1;
+      double z2 = -aNormDist[((ih + 1) * nw + (iw + 1))*4+3] / ntKix2;
+      double z3 = -aNormDist[((ih + 1) * nw + (iw + 0))*4+3] / ntKix3;
       ::glTexCoord2d((iw + 0.5) / nw, (ih + 0.5) / nh);
       ::glVertex3d(z0 * (iw + 0.5), z0 * (ih + 0.5), z0);
       ::glTexCoord2d((iw + 1.5) / nw, (ih + 0.5) / nh);
