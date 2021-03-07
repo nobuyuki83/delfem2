43f1abda6a55cfbbd4b8c5d82029fe2a5ce75420
nobuyuki83 <n.umetani@gmail.com>
2021-03-07 13:42:35 +0900

    github actoin for windows alembic build

--- scripts/build_win.bat
+++ scripts/build_win.bat
@@ -4,7 +4,10 @@ git submodule update --init --recursive
 : ################################
 : test cpp
 
+git submodule update --init -- 3rd_party/googletest
 cd 3rd_party/googletest
+git checkout master
+git pull origin master
 mkdir buildVS64
 cd buildVS64
 cmake -A x64 -Dgtest_force_shared_crt=ON ..
@@ -81,47 +84,65 @@ copy zconf.h ..
 cd ../../../
 
 : ##############################
-: glfw_smpl
+: glfw_cnpy
 
+set path_zlib_lib=%~dp0..\3rd_party\zlib\buildMake\Release\zlib.lib
+set path_zlib_inc=%~dp0..\3rd_party\zlib
 cd examples_oldgl_glfw_cnpy
 mkdir buildVS64Hdronly
 cd buildVS64Hdronly
 cmake .. -A x64 ^
   -DUSE_HEADERONLY=ON ^
-  -DZLIB_LIBRARY=%path3rdparty%\zlib\buildMake\Release\zlib.lib ^
-  -DZLIB_INCLUDE_DIR="%path3rdparty%\zlib"
+  -DZLIB_LIBRARY="%path_zlib_lib%" ^
+  -DZLIB_INCLUDE_DIR="%path_zlib_inc%"
 cmake --build . --config Release
 cd ../../
 
 : ##############################
 : build OpenEXR
 
+set path_zlib_library=%~dp0..\3rd_party\zlib\buildMake\Release\zlib.lib
+set path_zlib_root=%~dp0..\3rd_party\zlib
+set path3rdparty=%~dp0..\3rd_party
 cd 3rd_party
 curl -L https://github.com/AcademySoftwareFoundation/openexr/archive/v2.5.2.zip -o openexr.zip
 7z x openexr.zip -y
 cd openexr-2.5.2
 cmake . -A x64 ^ 
-  -DZLIB_ROOT=%zlib_root% ^
-  -DZLIB_LIBRARY=%zlib_library% ^
+  -DZLIB_ROOT=%path_zlib_root% ^
+  -DZLIB_LIBRARY=%path_zlib_library% ^
   -DPYILMBASE_ENABLE=OFF ^ 
   -DBUILD_TESTING=OFF
 cmake --build . --config Release
-mkdir %path3rdparty%\OpenEXR
-cmake --install . --prefix %path3rdparty%\OpenEXR
+mkdir %path3rdparty%\OpenEXR_Lib
+cmake --install . --prefix %path3rdparty%\OpenEXR_Lib
 cd ../../
 
 : ##############################
 : build Alembic
 
+set path3rdparty=%~dp0..\3rd_party
+
 git submodule update --init 3rd_party/alembic
+
 cd 3rd_party/alembic
 git checkout master
 git pull origin master
-: cmake -A x64 . -DUSE_TESTS=OFF -DALEMBIC_SHARED_LIBS=ON -DALEMBIC_ILMBASE_ROOT=%pathopenexr%\bin\Release -DILMBASE_INCLUDE_DIR=%pathopenexr%\bin\Release -DILMBASE_ROOT=%pathopenexr%\bin\Release
-: cmake -A x64 . -DUSE_TESTS=OFF -DALEMBIC_SHARED_LIBS=ON -DALEMBIC_ILMBASE_ROOT=%pathopenexr%\bin\Release -DILMBASE_ROOT=%pathopenexr%\bin\Release
 cmake -A x64 . ^
   -DUSE_TESTS=OFF ^
   -DALEMBIC_SHARED_LIBS=ON ^
-  -DILMBASE_ROOT=%path3rdparty%\OpenEXR 
+  -DILMBASE_ROOT=%path3rdparty%\OpenEXR_Lib
 cmake --build . --config Release
+mkdir %path3rdparty%\Alembic_Lib
+cmake --install . --prefix %path3rdparty%\Alembic_Lib
 cd ../../
+
+: ###############################
+: build examples alembic
+
+cd examples_alembic
+mkdir buildVS64Static
+cd buildVS64Static
+cmake -A x64 .. -DILMBASE_ROOT=%path3rdparty%\OpenEXR_Lib
+cmake --build .
+cd ../..
