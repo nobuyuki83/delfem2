f029728a78fa3650715f60af7ca3668aa98870c0
Nobuyuki Umetani <n.umetani@gmail.com>
2021-03-12 20:13:13 +0900

    updated emscripten demo htmls

--- scripts/build_macos_em.sh
+++ scripts/build_macos_em.sh
@@ -27,77 +27,77 @@ echo "################################"
 
 echo "00_openwin"
 cd docs/00_openwin
-em++ ../../examples_newgl_glfw/00_openwin/main.cpp -o index.html \
+em++ ../../examples_newgl_glfw/00_openwin/main.cpp -o index.js \
 	-std=c++1z \
 	-s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1 
 cd ../../
 
 echo "01_drawrect"
 cd docs/01_drawrect
-em++  ../../examples_newgl_glfw/01_drawrect/main.cpp -o index.html \
+em++  ../../examples_newgl_glfw/01_drawrect/main.cpp -o index.js \
 	-std=c++11 -DDFM2_HEADER_ONLY=ON  -I"../../include"  \
 	-s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1 
 cd ../../
 
 echo "02_nav3d"
 cd docs/02_nav3d
-em++  ../../examples_newgl_glfw/02_nav3d/main.cpp -o index.html \
+em++  ../../examples_newgl_glfw/02_nav3d/main.cpp -o index.js \
 	-std=c++11 -DDFM2_HEADER_ONLY=ON  -I"../../include" \
 	-s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1 
 cd ../../
 
 echo "03_texture"
 cd docs/03_texture
-em++  ../../examples_newgl_glfw/03_texture/main.cpp -o index.html \
+em++  ../../examples_newgl_glfw/03_texture/main.cpp -o index.js \
 	-std=c++11 -DDFM2_HEADER_ONLY=ON  -I"../../include"  \
 	-s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1 
 cd ../../
 
 echo "04_render2texture"
 cd docs/04_render2texture
-em++  ../../examples_newgl_glfw/04_render2texture/main.cpp -o index.html \
+em++  ../../examples_newgl_glfw/04_render2texture/main.cpp -o index.js \
 	-std=c++11 -DDFM2_HEADER_ONLY=ON  -I"../../include" \
 	-s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1 
 cd ../../
 
 echo "05_offscreenprojection"
 cd docs/05_offscreenprojection
-em++  ../../examples_newgl_glfw/05_OffScreenProjection/main.cpp -o index.html \
+em++  ../../examples_newgl_glfw/05_OffScreenProjection/main.cpp -o index.js \
 	-std=c++11 -DDFM2_HEADER_ONLY=ON  -I"../../include" \
 	-s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1
 cd ../../
 
 echo "20_cad2d"
 cd docs/20_cad2d
-em++  ../../examples_newgl_glfw/20_cad2d/main.cpp -o index.html \
+em++  ../../examples_newgl_glfw/20_cad2d/main.cpp -o index.js \
 	-std=c++11 -DDFM2_HEADER_ONLY=ON  -I"../../include" \
 	-s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1
 cd ../../
 
 echo "30_cadmsh2d"
 cd docs/30_cadmsh2d
-em++  ../../examples_newgl_glfw/30_cadmsh2d/main.cpp -o index.html \
+em++  ../../examples_newgl_glfw/30_cadmsh2d/main.cpp -o index.js \
 	-std=c++11 -DDFM2_HEADER_ONLY=ON  -I"../../include" \
 	-s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1
 cd ../../
 
 echo "40_femcloth"
 cd docs/40_femcloth
-em++  ../../examples_newgl_glfw/40_femcloth/main.cpp -o index.html \
+em++  ../../examples_newgl_glfw/40_femcloth/main.cpp -o index.js \
 	-std=c++11 -DDFM2_HEADER_ONLY=ON  -I"../../include" \
 	-O2 -s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1
 cd ../../
 
 echo "41_fem2d_poisson"
 cd docs/41_fem2d_poisson
-em++  ../../examples_newgl_glfw/41_fem2d_poisson/main.cpp -o index.html \
+em++  ../../examples_newgl_glfw/41_fem2d_poisson/main.cpp -o index.js \
 	-std=c++11 -DDFM2_HEADER_ONLY=ON  -I"../../include" \
 	-s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1
 cd ../../
 
 echo "42_fem2d_linearsolid"
 cd docs/42_fem2d_linearsolid
-em++  ../../examples_newgl_glfw/42_fem2d_linearsolid/main.cpp -o index.html \
+em++  ../../examples_newgl_glfw/42_fem2d_linearsolid/main.cpp -o index.js \
 	-std=c++11 -DDFM2_HEADER_ONLY=ON  -I"../../include" \
 	-s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1
 cd ../../
@@ -132,7 +132,7 @@ em++  ../../examples_newgl_glfw_imgui/10_imgui_basic/main.cpp \
 	../../3rd_party/imgui/backends/imgui_impl_opengl3.cpp \
 	../../3rd_party/imgui/imgui_tables.cpp \
 	../../3rd_party/imgui/imgui_widgets.cpp \
-	-o index.html \
+	-o index.js \
 	-std=c++11 -DDFM2_HEADER_ONLY=ON  -I"../../include"  \
 	-I"../../3rd_party/imgui" -I"../../3rd_party/imgui/backends" \
 	-s USE_WEBGL2=1 -s USE_GLFW=3 -s WASM=1 
