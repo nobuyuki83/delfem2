44f28f0f743e39e0814682c1aefe6b35b77130b6
Nobuyuki Umetani <n.umetani@gmail.com>
2021-09-04 12:26:24 +0900

    rename polygon2 file

--- examples_oldgl_glfw/625_Fem2MasterSlave/main.cpp
+++ examples_oldgl_glfw/625_Fem2MasterSlave/main.cpp
@@ -31,7 +31,7 @@
 #include "delfem2/dtri.h"
 #include "delfem2/color.h"
 #include "delfem2/jagarray.h"
-#include "delfem2/geoplygn2_v2.h"
+#include "delfem2/geo_polygon2.h"
 #include "delfem2/glfw/viewer3.h"
 #include "delfem2/glfw/util.h"
 #include "delfem2/opengl/old/color.h"
