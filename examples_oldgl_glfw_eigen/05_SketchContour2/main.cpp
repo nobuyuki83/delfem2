40cb3ae5c2363ceed5d548f939d764a0fc5b1a2b
Nobuyuki Umetani <n.umetani@gmail.com>
2021-10-17 13:52:50 +0900

    make file for rbf approximation

--- examples_oldgl_glfw_eigen/05_SketchContour2/main.cpp
+++ examples_oldgl_glfw_eigen/05_SketchContour2/main.cpp
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#include <utility>
 #include <vector>
 #include <cstdlib>
 #if defined(_WIN32) // windows
@@ -13,7 +14,6 @@
 #endif
 #define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
-#include <Eigen/LU>
 
 #include "delfem2/vec2.h"
 #include "delfem2/points.h"
@@ -25,82 +25,16 @@
 #include "delfem2/glfw/util.h"
 #include "delfem2/opengl/old/mshuni.h"
 #include "delfem2/opengl/old/color.h"
+#include "delfem2/eigen/implicit_rbf_approximation.h"
 
-template <class V2>
-void solveSignFieldRBF (
-    std::vector<double>& aWeight,
-    std::vector<V2>& stroke_offset,
-    const std::vector<V2>& stroke,
-    double eps)
-{
-  const size_t n = stroke.size();
-  std::vector<V2> aNorm;
-  aNorm.resize(n, V2(0, 0));
-  for(unsigned int is=0;is<stroke.size()-1;is++){
-    V2 p0 = stroke[is+0];
-    V2 p1 = stroke[is+1];
-    V2 e = (p1-p0).normalized();
-    V2 v(e.y, -e.x);
-    aNorm[is+0] += v;
-    aNorm[is+1] += v;
-  }
-  for(unsigned int i=0;i<n;i++){ aNorm[i].normalize(); }
-
-  stroke_offset.resize(n*2);
-  for(unsigned int i=0;i<n*2;i++){
-    unsigned int i0 = i/2;
-    V2 pi = stroke[i0];
-    pi += (i%2==0) ? aNorm[i0]*eps : -1.0*aNorm[i0]*eps;
-    stroke_offset[i] = pi;
-  }
-  // ---------
-  Eigen::MatrixXd A(2*n,2*n);
-  A.setZero();
-  Eigen::VectorXd y(2*n);
-  for(unsigned int i=0;i<n*2;i++){
-    for(unsigned int j=0;j<n*2;j++){
-      V2 pi = stroke_offset[i];
-      V2 pj = stroke_offset[j];
-      double r = (pi-pj).norm();
-      A(i,j) = exp(-r);
-//      if( i != j ) { A(i, j) = r * r * log(r); }
-    }
-    y(i) = (i%2==0) ? eps : -eps;
-  }
-  Eigen::FullPivLU<Eigen::MatrixXd> solver(A);
-  Eigen::VectorXd x = solver.solve(y);
-  aWeight.resize(2*n);
-  for(unsigned int i=0;i<2*n;i++){
-    aWeight[i] = x(i);
-  }
-}
-
-template <class V2>
-double evaluateRBF(
-    const V2& p,
-    const std::vector<double>& aWeight,
-    const std::vector<V2>& stroke_offset)
-{
-  int n = (int)aWeight.size();
-  double t = 0;
-  for(int i=0;i<n;i++){
-    V2 pi = stroke_offset[i];
-    double r = (p-pi).norm();
-    t += aWeight[i]*exp(-r);
-    //t += aWeight[i]*r*r*log(r);
-  }
-  return t;
-}
-
-
-
-// ---------------------------
+// ----------------------
 
 void Draw(
     const std::vector<double> &vtx_xy,
     const std::vector<unsigned int> &tri_vtx,
     const std::vector<double> &vtx_val,
-    const std::vector<delfem2::CSegInfo> &aSeg) {
+    const std::vector<delfem2::CSegInfo> &aSeg,
+    const std::vector<delfem2::CVec2d> &stroke) {
   ::glDisable(GL_LIGHTING);
   ::glLineWidth(1);
   delfem2::opengl::DrawMeshTri2D_Edge(
@@ -109,7 +43,7 @@ void Draw(
   std::vector<std::pair<double, delfem2::CColor> > colorMap;
   delfem2::ColorMap_BlueCyanGreenYellowRed(
       colorMap,
-      -0.01, +0.01);
+      -1, +1);
   delfem2::opengl::DrawMeshTri2D_ScalarP1(
       vtx_xy.data(), vtx_xy.size() / 2,
       tri_vtx.data(), tri_vtx.size() / 3,
@@ -118,7 +52,7 @@ void Draw(
   ::glLineWidth(5);
   ::glBegin(GL_LINES);
   ::glColor3d(0, 0, 0);
-  for (auto &iseg : aSeg) {
+  for (auto &iseg: aSeg) {
     double pA[2], pB[2];
     iseg.Pos2D(pA, pB,
                vtx_xy.data(), tri_vtx.data());
@@ -126,7 +60,14 @@ void Draw(
     ::glVertex2dv(pB);
   }
   ::glEnd();
-
+  // ---
+  ::glColor3d(1, 0, 0);
+  ::glPointSize(5);
+  ::glBegin(GL_POINTS);
+  for (auto &ip: stroke) {
+    ::glVertex3d(ip.x, ip.y, 0.1);
+  }
+  ::glEnd();
 }
 
 void Initialize(
@@ -155,42 +96,67 @@ int main() {
       vtx_xy, tri_vtx,
       32);
   delfem2::glfw::CViewer3 viewer;
-
-  //
   delfem2::glfw::InitGLOld();
   viewer.InitGL();
+  // ---------------------
+  double time0 = glfwGetTime();
+  glfwSetWindowTitle(viewer.window, "rbf without linear");
   while (!glfwWindowShouldClose(viewer.window)) {
-    std::vector<delfem2::CSegInfo> segments;
-    std::vector<double> vtx_val;
-    {
-      double time0 = glfwGetTime();
-      auto ndiv = static_cast<unsigned int>(28. * sin(time0) + 30.0);
-      std::vector<delfem2::CVec2d> stroke;
-      for(unsigned int i=0;i<ndiv;++i){
-        stroke.emplace_back(0.3*sin(i*0.1),0.3*cos(i*0.1));
-      }
-      std::vector<double> aWeight;
-      std::vector<delfem2::CVec2d> stroke_offset;
-      solveSignFieldRBF (
-          aWeight,
-          stroke_offset, stroke, 1.0e-3);
-      vtx_val.resize(vtx_xy.size()/2);
-      for(unsigned int ip=0;ip<vtx_xy.size()/2;++ip){
-        vtx_val[ip] = evaluateRBF(
-            delfem2::CVec2d(vtx_xy[ip*2+0], vtx_xy[ip*2+1]),
-            aWeight, stroke_offset);
-      }
-      delfem2::AddContour(
-          segments,
-          0.0,
-          tri_vtx.data(), tri_vtx.size() / 3,
-          vtx_val.data());
+    std::vector<delfem2::CVec2d> stroke;
+    ImplicitRbfApproximation rbf(
+        [](double r) { return std::exp(-r); },
+        false);
+    auto ndiv = static_cast<unsigned int>(28. * sin(glfwGetTime()) + 30.0);
+    for (unsigned int i = 0; i < ndiv; ++i) {
+      stroke.emplace_back(0.3 * sin(i * 0.1), 0.4 * cos(i * 0.1));
     }
+    rbf.SetPolyline2(stroke, 1.0e-3);
+    std::vector<double> vtx_val(vtx_xy.size() / 2, 0.0);
+    for (unsigned int ip = 0; ip < vtx_xy.size() / 2; ++ip) {
+      vtx_val[ip] = rbf.Evaluate2(vtx_xy[ip * 2 + 0], vtx_xy[ip * 2 + 1]);
+    }
+    std::vector<delfem2::CSegInfo> segments;
+    delfem2::AddContour(
+        segments,
+        0.0,
+        tri_vtx.data(), tri_vtx.size() / 3,
+        vtx_val.data());
     // ------------------
     viewer.DrawBegin_oldGL();
-    Draw(vtx_xy, tri_vtx, vtx_val, segments);
+    Draw(vtx_xy, tri_vtx, vtx_val, segments, stroke);
     viewer.SwapBuffers();
     glfwPollEvents();
+    if( glfwGetTime() - time0 > 6 ){ break; }
+  }
+  // -----------------------
+  time0 = glfwGetTime();
+  glfwSetWindowTitle(viewer.window, "rbf with linear");
+  while (!glfwWindowShouldClose(viewer.window)) {
+    std::vector<delfem2::CVec2d> stroke;
+    ImplicitRbfApproximation rbf(
+        [](double r) { return std::exp(-r); },
+        true);
+    auto ndiv = static_cast<unsigned int>(28. * sin(glfwGetTime()) + 30.0);
+    for (unsigned int i = 0; i < ndiv; ++i) {
+      stroke.emplace_back(0.3 * sin(i * 0.1), 0.4 * cos(i * 0.1));
+    }
+    rbf.SetPolyline2(stroke, 1.0e-3);
+    std::vector<double> vtx_val(vtx_xy.size() / 2, 0.0);
+    for (unsigned int ip = 0; ip < vtx_xy.size() / 2; ++ip) {
+      vtx_val[ip] = rbf.Evaluate2(vtx_xy[ip * 2 + 0], vtx_xy[ip * 2 + 1]);
+    }
+    std::vector<delfem2::CSegInfo> segments;
+    delfem2::AddContour(
+        segments,
+        0.0,
+        tri_vtx.data(), tri_vtx.size() / 3,
+        vtx_val.data());
+    // ------------------
+    viewer.DrawBegin_oldGL();
+    Draw(vtx_xy, tri_vtx, vtx_val, segments, stroke);
+    viewer.SwapBuffers();
+    glfwPollEvents();
+    if( glfwGetTime() - time0 > 6 ){ break; }
   }
   glfwDestroyWindow(viewer.window);
   glfwTerminate();
---
+++ include/delfem2/eigen/implicit_rbf_approximation.h
@@ -0,0 +1,106 @@
+//
+// Created by Nobuyuki Umetani on 2021-10-17.
+//
+
+#ifndef IMPLICIT_RBF_APPROXIMATION_H_
+#define IMPLICIT_RBF_APPROXIMATION_H_
+
+#include <functional>
+#include <vector>
+#include <Eigen/LU>
+
+class ImplicitRbfApproximation {
+ public:
+  explicit ImplicitRbfApproximation(
+      std::function<double(double)> rbf,
+      bool is_linear)
+      : rbf(std::move(rbf)), is_linear(is_linear) {}
+
+  template<class V2>
+  void SetPolyline2(const std::vector<V2> &stroke, double eps) {
+    GenerateSamplePositionsForPolyline(stroke, eps);
+    const auto num_sample = static_cast<unsigned int>(sample_xy.size() / 2);
+    const unsigned int ndof = is_linear ? num_sample + 3 : num_sample;
+    Eigen::MatrixXd A(ndof, ndof);
+    Eigen::VectorXd y(ndof);
+    A.setZero();
+    y.setZero();
+    for (unsigned int i = 0; i < num_sample; i++) {
+      for (unsigned int j = 0; j < num_sample; j++) {
+        const V2 pi(sample_xy[i * 2 + 0], sample_xy[i * 2 + 1]);
+        const V2 pj(sample_xy[j * 2 + 0], sample_xy[j * 2 + 1]);
+        double r = (pi - pj).norm();
+        A(i, j) = rbf(r);
+      }
+      y(i) = (i % 2 == 0) ? eps : -eps;
+    }
+    if (is_linear) {
+      for (unsigned int ismpl = 0; ismpl < num_sample; ismpl++) {
+        A(num_sample + 0, ismpl) = 1;
+        A(num_sample + 1, ismpl) = sample_xy[ismpl * 2 + 0];
+        A(num_sample + 2, ismpl) = sample_xy[ismpl * 2 + 1];
+        A(ismpl, num_sample + 0) = 1;
+        A(ismpl, num_sample + 1) = sample_xy[ismpl * 2 + 0];
+        A(ismpl, num_sample + 2) = sample_xy[ismpl * 2 + 1];
+      }
+    }
+    Eigen::FullPivLU<Eigen::MatrixXd> solver(A);
+    Eigen::VectorXd x = solver.solve(y);
+    weights.resize(ndof);
+    for (unsigned int idof = 0; idof < ndof; idof++) {
+      weights[idof] = x(idof);
+    }
+  }
+
+  double Evaluate2(double x, double y) {
+    const size_t num_samples = sample_xy.size() / 2;
+    const unsigned int ndof = is_linear ? num_samples + 3 : num_samples;
+    assert(weights.size() == ndof);
+    double t = 0;
+    for (unsigned int i = 0; i < num_samples; i++) {
+      double dx0 = sample_xy[i * 2 + 0] - x;
+      double dy0 = sample_xy[i * 2 + 1] - y;
+      double r = std::sqrt(dx0 * dx0 + dy0 * dy0);
+      t += weights[i] * rbf(r);
+    }
+    if (is_linear) {
+      t += weights[num_samples] + weights[num_samples + 1] * x + weights[num_samples + 2] * y;
+    }
+    return t;
+  }
+
+ private:
+  template<class V2>
+  void GenerateSamplePositionsForPolyline(const std::vector<V2> &stroke, double eps) {
+    const size_t n = stroke.size();
+    const size_t num_sample = n * 2;
+    std::vector<V2> aNorm;
+    aNorm.resize(n, V2(0, 0));
+    for (unsigned int is = 0; is < stroke.size() - 1; is++) {
+      V2 p0 = stroke[is + 0];
+      V2 p1 = stroke[is + 1];
+      V2 e = (p1 - p0).normalized();
+      V2 v(e.y, -e.x);
+      aNorm[is + 0] += v;
+      aNorm[is + 1] += v;
+    }
+    for (unsigned int i = 0; i < n; i++) { aNorm[i].normalize(); }
+    sample_xy.resize(num_sample * 2);
+    for (unsigned int ismpl = 0; ismpl < num_sample; ismpl++) {
+      unsigned int i0 = ismpl / 2;
+      V2 pi = stroke[i0];
+      pi += (ismpl % 2 == 0) ? aNorm[i0] * eps : -1.0 * aNorm[i0] * eps;
+      sample_xy[ismpl * 2 + 0] = pi[0];
+      sample_xy[ismpl * 2 + 1] = pi[1];
+    }
+  }
+ private:
+  const std::function<double(double)> rbf;
+  const bool is_linear;
+  //
+  std::vector<double> sample_xy;
+  std::vector<double> weights;
+};
+
+
+#endif //IMPLICIT_RBF_APPROXIMATION_H_
