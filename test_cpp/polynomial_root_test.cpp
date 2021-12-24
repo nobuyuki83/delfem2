671d3df5851c57589d893c82ed3625c4f1f65cc9
Nobuyuki <n.umetani@gmail.com>
2021-12-24 17:27:15 +0900

    added more tests

--- test_cpp/polynomial_root_test.cpp
+++ test_cpp/polynomial_root_test.cpp
@@ -80,6 +80,7 @@ TEST(polynomial_root, cubic_3roots) {
     const double b = -a * (x0 + x1 + x2);
     const double c = a * (x0 * x1 + x1 * x2 + x2 * x0);
     const double d = -a * x0 * x1 * x2;
+    const auto coeff = std::array<double, 4>{d, c, b, a};
     double strum[4][4];
     delfem2::StrumSequence_CubicPolynomial(strum, a, b, c, d);
     int nroot1 = delfem2::StrumNumber<4>(0, strum) - delfem2::StrumNumber<4>(1, strum);
@@ -98,9 +99,9 @@ TEST(polynomial_root, cubic_3roots) {
         bool b1 = (intvl.first - x1) * (intvl.second - x1) < 0;
         bool b2 = (intvl.first - x2) * (intvl.second - x2) < 0;
         EXPECT_TRUE(b0 || b1 || b2);
+        // the number of roots should be 1 in the interval
         int nr = delfem2::StrumNumber<4>(intvl.first, strum) - delfem2::StrumNumber<4>(intvl.second, strum);
         EXPECT_EQ(nr, 1);
-        auto coeff = std::array<double, 4>{d, c, b, a};
         double v0 = delfem2::Eval_Polynomial(intvl.first, coeff.data(), 4);
         double v1 = delfem2::Eval_Polynomial(intvl.second, coeff.data(), 4);
         EXPECT_LT(v0 * v1, 0.);
@@ -132,8 +133,9 @@ TEST(polynomial_root, quartic_4roots) {
     const double c = +a * (x0 * x1 + x0 * x2 + x0 * x3 + x1 * x2 + x1 * x3 + x2 * x3);
     const double d = -a * (x0 * x1 * x2 + x1 * x2 * x3 + x2 * x3 * x0 + x3 * x0 * x1);
     const double e = +a * x0 * x1 * x2 * x3;
+    const auto coeff = std::array<double, 5>{e, d, c, b, a};
     double strum[5][5];
-    delfem2::StrumSequenceOfPolynomial<5>(strum, std::array<double,5>{e,d,c,b,a}.data() );
+    delfem2::StrumSequenceOfPolynomial<5>(strum, coeff.data());
     int nroot1 = delfem2::StrumNumber<5>(0, strum) - delfem2::StrumNumber<5>(1, strum);
     {
       int nroot0 = 0;
@@ -142,6 +144,22 @@ TEST(polynomial_root, quartic_4roots) {
       if (x2 > 0 && x2 < 1) { nroot0++; }
       if (x3 > 0 && x3 < 1) { nroot0++; }
       EXPECT_EQ(nroot0, nroot1);
+    }
+    {
+      std::vector<std::pair<double, double>> intvls = delfem2::RootInterval_StrumSequence<5>(0, 1, strum);
+      EXPECT_EQ(intvls.size(), nroot1);
+      for (auto intvl: intvls) {
+        bool b0 = (intvl.first - x0) * (intvl.second - x0) < 0;
+        bool b1 = (intvl.first - x1) * (intvl.second - x1) < 0;
+        bool b2 = (intvl.first - x2) * (intvl.second - x2) < 0;
+        bool b3 = (intvl.first - x3) * (intvl.second - x3) < 0;
+        EXPECT_TRUE(b0 || b1 || b2 || b3);
+        const int nr = delfem2::StrumNumber<5>(intvl.first, strum) - delfem2::StrumNumber<5>(intvl.second, strum);
+        EXPECT_EQ(nr, 1);
+        const double v0 = delfem2::Eval_Polynomial(intvl.first, coeff.data(), 5);
+        const double v1 = delfem2::Eval_Polynomial(intvl.second, coeff.data(), 5);
+        EXPECT_LT(v0 * v1, 0.);
+      }
     }
   }
 }
@@ -175,10 +193,14 @@ TEST(polynomial_root, quintic_5roots) {
       + x0 * x1 * x2 * x4
       + x0 * x1 * x2 * x3);
     const double f = -a * x0 * x1 * x2 * x3 * x4;
+    const auto coeff = std::array<double, 6>{f, e, d, c, b, a};
+    // F(x)
+    // = a*(x-x0)*(x-x1)*(x-x2)*(x-x3)*(x-x4)
+    // = a * x^5 + b * x^4 + c * x^3 + d * x^2 + e * x + f
     double strum[6][6];
-    delfem2::StrumSequenceOfPolynomial<6>(strum, std::array<double,6>{f,e,d,c,b,a}.data() );
+    delfem2::StrumSequenceOfPolynomial<6>(strum, coeff.data() );
     int nroot1 = delfem2::StrumNumber<6>(0, strum) - delfem2::StrumNumber<6>(1, strum);
-    { // check the number of roots
+    {  // check the number of roots
       int nroot0 = 0;
       if (x0 > 0 && x0 < 1) { nroot0++; }
       if (x1 > 0 && x1 < 1) { nroot0++; }
@@ -186,6 +208,23 @@ TEST(polynomial_root, quintic_5roots) {
       if (x3 > 0 && x3 < 1) { nroot0++; }
       if (x4 > 0 && x4 < 1) { nroot0++; }
       EXPECT_EQ(nroot0, nroot1);
+    }
+    {
+      std::vector<std::pair<double, double>> intvls = delfem2::RootInterval_StrumSequence<6>(0, 1, strum);
+      EXPECT_EQ(intvls.size(), nroot1);
+      for (auto intvl: intvls) {
+        const bool b0 = (intvl.first - x0) * (intvl.second - x0) < 0;
+        const bool b1 = (intvl.first - x1) * (intvl.second - x1) < 0;
+        const bool b2 = (intvl.first - x2) * (intvl.second - x2) < 0;
+        const bool b3 = (intvl.first - x3) * (intvl.second - x3) < 0;
+        const bool b4 = (intvl.first - x4) * (intvl.second - x4) < 0;
+        EXPECT_TRUE(b0 || b1 || b2 || b3 || b4);
+        const int nr = delfem2::StrumNumber<6>(intvl.first, strum) - delfem2::StrumNumber<6>(intvl.second, strum);
+        EXPECT_EQ(nr, 1);
+        const double v0 = delfem2::Eval_Polynomial(intvl.first, coeff.data(), 6);
+        const double v1 = delfem2::Eval_Polynomial(intvl.second, coeff.data(), 6);
+        EXPECT_LT(v0 * v1, 0.);
+      }
     }
   }
 }
